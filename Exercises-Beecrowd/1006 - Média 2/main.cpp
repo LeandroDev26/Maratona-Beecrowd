6d0d9640ba6b70f4dc398b8a15aa259eb9111867
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-02-25 19:02:12 -0300

    update

---
+++ Exercises-Beecrowd/1006 - Média 2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     double a ,b ,c ,media;
+    cin>> a >> b >> c;
+       media = (a*2 + b*3 + c*5)/10;
+        cout<<"MEDIA = "<< media<<endl;
+    return 0;
+}
