2dd9743c6798193e36fda46d1991b34c6b121cad
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-02-25 18:52:25 -0300

    update

---
+++ Exercises-Beecrowd/1004 - Produto Simples/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int y , x ,PROD;
+  cin>> x >> y;
+        PROD = x * y;
+         cout<<"PROD = " << PROD <<endl;
+
+
+
+return 0;
+
+}
