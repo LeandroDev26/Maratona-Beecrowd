89bcc15ef1487d7682152268c0c3abf69f976cd3
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-02-26 19:03:12 -0300

    update

---
+++ Exercises-Beecrowd/1016 - Distância/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int D;
+    cin>> D;
+    int temp;
+    temp = D*2;
+    cout<< temp << " minutos"<<"\n";
+
+    return 0;
+}
