7cae2a48dc1df7ba65fb8f8719a93d6ec9cd1b9c
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-02-26 20:55:49 -0300

    update

---
+++ Exercises-Beecrowd/1037 - Intervalo/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+
+   double  x;
+   cin>> x;
+
+   cout<< fixed << setprecision(2);
+
+   if( x >= 0 && x <= 25){
+
+       cout<< "Intervalo [0,25]"<<endl;
+   }
+
+      if( x > 25 && x <= 50){
+
+       cout<< "Intervalo (25,50]"<<endl;
+   }
+
+      if( x > 50 && x <= 75){
+
+       cout<< "Intervalo (50,75]"<<endl;
+   }
+
+     if( x > 75 && x <= 100){
+
+       cout<< "Intervalo (75,100]"<<endl;
+   }
+
+   if(x < 0 || x > 100){
+       cout<<"Fora de intervalo"<<endl;
+   }
+    return 0;
+}
