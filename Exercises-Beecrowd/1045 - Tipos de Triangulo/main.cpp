0c68614ca1234a222a4e8c3a60f2b315fd20602d
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-02 22:56:28 -0300

    feat

---
+++ Exercises-Beecrowd/1045 - Tipos de Triangulo/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void ordenar (double a , double b , double c) {
+       if (b>a){
+        int temp = a;
+        a = b;
+        b = temp;
+       }
+
+}
+
+int main()
+{
+    double a, b, c;
+
+
+
+    cin>> a >> b >> c;
+
+
+
+
+    return 0;
+}
