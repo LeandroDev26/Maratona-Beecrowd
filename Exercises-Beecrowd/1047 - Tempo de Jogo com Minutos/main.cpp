054c64051899e9ed2955e25b449ce136f2012579
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-05 19:10:24 -0300

    feat

---
+++ Exercises-Beecrowd/1047 - Tempo de Jogo com Minutos/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int hi, hf, mi, mf, hr;
+
+    cin>> hi >> mi >> hf >> mf;
+
+
+
+    if(hi == hf)
+    {
+        cout<< "O JOGO DUROU " << 24 <<" HORA(S)" << endl;
+    }
+    else if(hf > hi)
+    {
+        hr = hf - hi;
+        cout<< "O JOGO DUROU " << hr <<" HORA(S)" << endl;
+
+    }
+    else
+    {
+        hr = (24 - hi) + hf;
+        cout<< "O JOGO DUROU " << hr <<" HORA(S)" << endl;
+
+    }
+
+
+
+    return 0;
+}
