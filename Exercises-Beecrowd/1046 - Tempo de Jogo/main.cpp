92a4ca5bd82c522792b127818a44c9c5006f7e9a
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-04 21:10:52 -0300

    feat

--- Exercises-Beecrowd/1046 - Tempo de Jogo/main.cpp
+++ Exercises-Beecrowd/1046 - Tempo de Jogo/main.cpp
@@ -6,11 +6,19 @@ int main()
 {
     int hi, hf, hr;
 
+    hr = (24 - hi) - (24 - hf);
+
     cin>> hi >> hf;
 
     if(hi == 0 && hf == 0)
     {
         cout<< "O JOGO DUROU " << 24 <<" HORA(S)" << endl;
+    }
+    else
+    {
+
+        cout<< "O JOGO DUROU " << hr <<" HORA(S)" << endl;
+
     }
 
 
