48de3cebf671d031cd37b2e30d2f84e8e77aa750
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-09 19:06:01 -0300

    feat

--- Exercises-Beecrowd/1048 - Aumento de Salario/main.cpp
+++ Exercises-Beecrowd/1048 - Aumento de Salario/main.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main()
 {
-   double s;
+   double s , p;
    cin>> s;
 
    if(s > 0 && s <= 400.00){
 
-     cout<< "Novo salario: "<<  <<endl;
+      p = s*0.15;
+      s += p;
+     cout<< "Novo salario: "<< s <<endl;
    }
 
 }
