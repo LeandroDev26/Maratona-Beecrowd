6308b434c0bf9d413c14804e1082ca5e2106b7cc
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-10 20:28:08 -0300

    feat

--- Exercises-Beecrowd/1049 - Animal/main.cpp
+++ Exercises-Beecrowd/1049 - Animal/main.cpp
@@ -7,23 +7,32 @@ int main()
     string a, b,c;
     cin>> a >> b >> c;
 
-    if (a == "vertebrado") {
-    if (b == "ave") {
-        if (c == "carnivoro") cout << "aguia" << endl;
-        else cout << "pomba" << endl;
-    } else { // mamifero
-        if (c == "onivoro") cout << "homem" << endl;
-        else cout << "vaca" << endl;
+    if (a == "vertebrado")
+    {
+        if (b == "ave")
+        {
+            if (c == "carnivoro") cout << "aguia" << endl;
+            else cout << "pomba" << endl;
+        }
+        else     // mamifero
+        {
+            if (c == "onivoro") cout << "homem" << endl;
+            else cout << "vaca" << endl;
+        }
     }
-} else { // invertebrado
-    if (b == "inseto") {
-        if (c == "hematofago") cout << "pulga" << endl;
-        else cout << "lagarta" << endl;
-    } else { // anelideo
-        if (c == "hematofago") cout << "sanguesuga" << endl;
-        else cout << "minhoca" << endl;
+    else     // invertebrado
+    {
+        if (b == "inseto")
+        {
+            if (c == "hematofago") cout << "pulga" << endl;
+            else cout << "lagarta" << endl;
+        }
+        else     // anelideo
+        {
+            if (c == "hematofago") cout << "sanguesuga" << endl;
+            else cout << "minhoca" << endl;
+        }
     }
-}
 
     return 0;
 }
