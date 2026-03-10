7b7438a66c3f654cd12f7380cfb3183869fbcdcf
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-10 20:27:49 -0300

    feat

--- Exercises-Beecrowd/1049 - Animal/main.cpp
+++ Exercises-Beecrowd/1049 - Animal/main.cpp
@@ -7,39 +7,23 @@ int main()
     string a, b,c;
     cin>> a >> b >> c;
 
-    if(a == "vertebrado" && b == "ave" && c == "carnivoro")
-    {
-        cout<<"aguia"<<endl;
+    if (a == "vertebrado") {
+    if (b == "ave") {
+        if (c == "carnivoro") cout << "aguia" << endl;
+        else cout << "pomba" << endl;
+    } else { // mamifero
+        if (c == "onivoro") cout << "homem" << endl;
+        else cout << "vaca" << endl;
     }
-
-    else if(a == "vertebrado" && b == "ave" && c == "onivoro")
-    {
-        cout<<"pomba"<<endl;
+} else { // invertebrado
+    if (b == "inseto") {
+        if (c == "hematofago") cout << "pulga" << endl;
+        else cout << "lagarta" << endl;
+    } else { // anelideo
+        if (c == "hematofago") cout << "sanguesuga" << endl;
+        else cout << "minhoca" << endl;
     }
-    else if(a == "vertebrado" && b == "mamifero" && c == "onivoro")
-    {
-        cout<<"homem"<<endl;
-    }
-    else if(a == "vertebrado" && b == "mamifero" && c == "herbivoro")
-    {
-        cout<<"vaca"<<endl;
-    }
-    else if(a == "invertebrado" && b == "inseto" && c == "hematofago")
-    {
-        cout<<"pulga"<<endl;
-    }
-    else if(a == "invertebrado" && b == "inseto" && c == "herbivoro")
-    {
-        cout<<"lagarta"<<endl;
-    }
-    else if(a == "invertebrado" && b == "anelideo" && c == "hematofago")
-    {
-        cout<<"sanguesuga"<<endl;
-    }
-    else if(a == "invertebrado" && b == "inseto" && c == "onivoro")
-    {
-        cout<<"minhoca"<<endl;
-    }
+}
 
     return 0;
 }
