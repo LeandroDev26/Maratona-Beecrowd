d65b815379f09969249dbd3eee5a685fff035fda
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-12 20:35:35 -0300

    feat

--- Exercises-Beecrowd/1051 - Imposto de Renda/main.cpp
+++ Exercises-Beecrowd/1051 - Imposto de Renda/main.cpp
@@ -19,7 +19,39 @@ int main()
         if( s > 1000.00)
         {
             s -= 1000.00;
-            double fatia = (s*0.18);
+            double fatia = (s*0.18);#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double s, imposto = 0;
+    cin >> s;
+
+    cout << fixed << setprecision(2);
+
+    if (s <= 2000.00) {
+        cout << "Isento" << endl;
+    } else {
+        if (s > 4500.00) {
+            // Taxa de 28% sobre o que excede 4500
+            imposto += (s - 4500.00) * 0.28;
+            s = 4500.00; // O que restou para as outras faixas é o limite dela
+        }
+        if (s > 3000.00) {
+            // Taxa de 18% sobre o que excede 3000 (até 4500)
+            imposto += (s - 3000.00) * 0.18;
+            s = 3000.00;
+        }
+        if (s > 2000.00) {
+            // Taxa de 8% sobre o que excede 2000 (até 3000)
+            imposto += (s - 2000.00) * 0.08;
+        }
+
+        cout << "R$ " << imposto << endl;
+    }
+
+    return 0;
+}
             imposto += fatia;
         }
         else
