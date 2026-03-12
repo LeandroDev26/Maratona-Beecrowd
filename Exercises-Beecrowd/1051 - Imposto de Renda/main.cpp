5ba9957c04ea8a55b611036309cd8dafdfdd9105
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-12 20:38:38 -0300

    feat

--- Exercises-Beecrowd/1051 - Imposto de Renda/main.cpp
+++ Exercises-Beecrowd/1051 - Imposto de Renda/main.cpp
@@ -2,27 +2,6 @@
 #include <iomanip>
 using namespace std;
 
-int main()
-{
-    double s, imposto;
-    cin>> s;
-    cout<< fixed << setprecision(2);
-    if( s > 0.00 && s <= 2000.00)
-    {
-        cout<< "Isento" <<endl;
-    }
-    else
-    {
-        s -= 2000.00;
-        double fatia = (s*0.08);
-        imposto = fatia;
-        if( s > 1000.00)
-        {
-            s -= 1000.00;
-            double fatia = (s*0.18);#include <iostream>
-#include <iomanip>
-using namespace std;
-
 int main() {
     double s, imposto = 0;
     cin >> s;
@@ -49,32 +28,6 @@ int main() {
 
         cout << "R$ " << imposto << endl;
     }
-
-    return 0;
-}
-            imposto += fatia;
-        }
-        else
-        {
-            cout<< "R$ "<< imposto <<endl;
-        }
-
-        if( s > 1500.00)
-        {
-            s -= 1500.00;
-            double fatia = (s*0.28);
-            imposto += fatia;
-
-            cout<< "R$ "<< imposto <<endl;
-        }
-        else
-        {
-            cout<< "R$ "<< imposto <<endl;
-
-        }
-
-    }
-
 
     return 0;
 }
