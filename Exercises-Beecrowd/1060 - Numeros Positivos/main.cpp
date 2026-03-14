fe3cb734ba62173a1cd1f07ba28ad5b74719c936
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-14 12:39:21 -0300

    feat

--- Exercises-Beecrowd/1060 - Numeros Positivos/main.cpp
+++ Exercises-Beecrowd/1060 - Numeros Positivos/main.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 int main()
 {
-     int a , b , c , d , e , f;
+    /* int a , b , c , d , e , f;
      cin>>  a >> b >> c >> d >> e >> f;
+
+     */
+
+    int a, cont =  0;
+    for(int i = 0; i < 5; i++)
+    {
+        cin>> a;
+        if(a > 0)
+        {
+            cont++;
+        }
+    }
+    cout<< cont << "valores positivos"<<endl;
+
     return 0;
 }
