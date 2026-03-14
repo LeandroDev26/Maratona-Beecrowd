78c3e95ca90ba4e6c19de63de9acf0790e325faf
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-14 15:28:18 -0300

    feat

--- Exercises-Beecrowd/1061 -Tempo de um Evento/main.cpp
+++ Exercises-Beecrowd/1061 -Tempo de um Evento/main.cpp
@@ -6,11 +6,16 @@ using namespace std;
 int main()
 {
     string lixo;
-    int diaIni , horaIni , minIni , segIni, diaFin , horaFin , minFin , segFin;
+    int diaIni , horaIni , minIni , segIni, diaFin , horaFin , minFin , segFin , totini , totfin;s
     cin>> lixo >> diaIni;
     cin>>horaIni >> lixo >> minIni >> lixo >> segIni;
     cin>> lixo >> diaFin;
     cin>>horaFin >> lixo >> minFin>> lixo >> segFin;
+
+    totini =  diaIni * 86400;
+    totini = diaIni * 3600;
+    totini = diaIni * 60;
+
 
 
 
