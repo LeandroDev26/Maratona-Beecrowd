24e243f54be8e4fc35ce6ec82ac97b226ff4c414
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-15 14:35:29 -0300

    feat

--- Exercises-Beecrowd/1065 - Pares entre Cinco Numeros/main.cpp
+++ Exercises-Beecrowd/1065 - Pares entre Cinco Numeros/main.cpp
@@ -4,16 +4,18 @@ using namespace std;
 
 int main()
 {
-   int e , cont;
-   for(int i = 0; i < 5; i++){
-    cin>> e ;
+    int e, cont;
+    for(int i = 0; i < 5; i++)
+    {
+        cin>> e ;
 
-    if ( e % 2 == 0){
-        cont++;
+        if ( e % 2 == 0)
+        {
+            cont++;
+        }
     }
-   }
 
-   cout<< cont << " valores pares"<<endl;
+    cout<< cont << " valores pares"<<endl;
 
 
     return 0;
