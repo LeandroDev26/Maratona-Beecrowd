94bcf306659c4babac39522165bc42e7d88f5bfb
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-15 20:28:34 -0300

    feat

--- Exercises-Beecrowd/1066 - Pares, Impares , Positivos e Negativos/main.cpp
+++ Exercises-Beecrowd/1066 - Pares, Impares , Positivos e Negativos/main.cpp
@@ -36,6 +36,8 @@ int main()
 
     cout<< pares << " valor(es) par(es)"<<endl;
         cout<< impares << " valor(es) impar(es)"<<endl;
+          cout<< positivo << "valor(es) positivo(s)"<<endl;
+
 
 
 
