210f58b890c6c2a0b5ad3464b3c7ff1738907e30
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-17 15:07:38 -0300

    feat

--- Exercises-Beecrowd/1074 - Par ou Impar/main.cpp
+++ Exercises-Beecrowd/1074 - Par ou Impar/main.cpp
@@ -30,6 +30,13 @@ int main()
         else if(valor_atual % 2 == 0 && valor_atual > 0)
             cout << "EVEN POSITIVE" << endl;
         }
+          else if(x % 2 != 0 && x < 0)
+        {
+            cout<<"ODD NEGATIVE"<<endl;
+        }
+
+
+
      return 0;
 
     }
