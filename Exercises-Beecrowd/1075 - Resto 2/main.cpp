849ed7b0e7958881a6232612b1426a834e501f32
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-17 15:38:24 -0300

    feat

--- Exercises-Beecrowd/1075 - Resto 2/main.cpp
+++ Exercises-Beecrowd/1075 - Resto 2/main.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 int main()
 {
-  int n;
-  cin>> n;
+    int n;
+    cin>> n;
 
-  for (int i = 1; i < 10000; i++){
-    if( i % n == 2){
+    for (int i = 1; i < 10000; i++)
+    {
+        if( i % n == 2)
+        {
 
-        cout<< i <<endl;
+            cout<< i <<endl;
+        }
     }
-  }
 
     return 0;
 }
