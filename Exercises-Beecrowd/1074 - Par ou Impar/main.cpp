44cba7b8630334058721f8984016f18d4277735e
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-17 14:37:27 -0300

    feat

--- Exercises-Beecrowd/1074 - Par ou Impar/main.cpp
+++ Exercises-Beecrowd/1074 - Par ou Impar/main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n , x;
     cin>> n;
     for (int i = 0; i < n; i++){
 
-            if (i == 0){
+            if (x == 0){
                 cout<<"NULL"<<endl;
             }
 
