b217249e6fa1912300bc70af3a9d32ca487c4054
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-17 13:56:48 -0300

    feat

--- Exercises-Beecrowd/1072 - Intervalo 2/main.cpp
+++ Exercises-Beecrowd/1072 - Intervalo 2/main.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int main()
 {
-  int n , x;
+  int n , x , in = 0 , out = 0;
   cin>> n;
 
   for(int i = 0; i < n; i++){
        cin>> x;
+       if(x >= 10 && x <=20){
+        in++;
+       }
   }
 
     return 0;
