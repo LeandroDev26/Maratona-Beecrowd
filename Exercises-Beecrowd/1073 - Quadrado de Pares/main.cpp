6618580e02974fb2f64755bb426fb6549228f25c
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-17 14:20:51 -0300

    feat

--- Exercises-Beecrowd/1073 - Quadrado de Pares/main.cpp
+++ Exercises-Beecrowd/1073 - Quadrado de Pares/main.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 int main()
 {
-   int n;
-   cin>> n;
+    int n;
+    cin>> n;
 
-   for(int i = 1; i <= n; i++){
-       if(i % 2 == 0){
-          cout<< i <<"^2 = " <<(i*i)<<endl;
-       }
-   }
+    for(int i = 1; i <= n; i++)
+    {
+        if(i % 2 == 0)
+        {
+            cout<< i <<"^2 = " <<(i*i)<<endl;
+        }
+    }
 
     return 0;
 }
