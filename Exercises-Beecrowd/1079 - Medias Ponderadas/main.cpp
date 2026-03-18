2c96e13efb690332d937c2697500e4bda3d9ea35
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-18 19:07:01 -0300

    feat

--- Exercises-Beecrowd/1079 - Medias Ponderadas/main.cpp
+++ Exercises-Beecrowd/1079 - Medias Ponderadas/main.cpp
@@ -12,8 +12,11 @@ int main()
   for( int i = 0; i < n; i++){
 
         cin>> a >> b >> c;
+        media = ((a*2)+(b*3)+(c*5))/ (2+3+5); // or 10
 
   }
-    media = ((a*2)+(b*3)+(c*5))/ (2+3+5); // or 10
+
+
+
     return 0;
 }
