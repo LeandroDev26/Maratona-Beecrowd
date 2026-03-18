f30b03b67c5ee1646f3de1bb69a5f4fc4e65d7e7
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-18 19:09:47 -0300

    feat

--- Exercises-Beecrowd/1079 - Medias Ponderadas/main.cpp
+++ Exercises-Beecrowd/1079 - Medias Ponderadas/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
@@ -10,10 +11,12 @@ int main()
   cin>> n;
   vector<int> results;
 
+  cout<< fixed << set
   for( int i = 0; i < n; i++){
 
         cin>> a >> b >> c;
         media = ((a*2)+(b*3)+(c*5))/ (2+3+5); // or 10
+        results.push_back(media);
 
   }
 
