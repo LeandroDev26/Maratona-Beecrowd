94afded778d7fd290bca8d1fbf299732b854f267
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-19 19:11:19 -0300

    feat

--- Exercises-Beecrowd/1095 - Sequencia IJ 1/main.cpp
+++ Exercises-Beecrowd/1095 - Sequencia IJ 1/main.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main()
 {
-  for(int i = 1; i < 60; i+=3){
+    for(int i = 1; i < 60; i+=3)
+    {
 
-      for(int j = 60; j > 0; j-=5){
+        for(int j = 60; j > 0; j-=5)
+        {
+            cout<< "I="<< i << "J=" << j <<endl;
 
-      }
+        }
 
-  }
+    }
 
 
     return 0;
