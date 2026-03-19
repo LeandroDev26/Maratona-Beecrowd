8277fbb07fa0621eba3f6a3c6941f27adb2e19a8
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-18 23:08:26 -0300

    feat

--- Exercises-Beecrowd/1080 - Maior e Posicao/main.cpp
+++ Exercises-Beecrowd/1080 - Maior e Posicao/main.cpp
@@ -6,17 +6,22 @@ using namespace std;
 int main()
 {
 
-  int e;
-  vector<int> numeros;
-  for (int i = 0; i < 100; i++){
+    int e, maior;
+    vector<int> numeros;
+    for (int i = 0; i < 100; i++)
+    {
 
         cin>> e;
         numeros.push_back(e);
 
-  }
-  for (int i = 0; i < numeros.size(); i++){
+    }
+    for (int i = 0; i < numeros.size(); i++)
+    {
+        if(numeros[i] > numeros[i+1]){
 
-  }
+        }
+
+    }
 
 
     return 0;
