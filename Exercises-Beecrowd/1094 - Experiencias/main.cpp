f8fe25d4f4722c623d7ec2a682f3f63c6570cd25
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-19 15:46:02 -0300

    feat

--- Exercises-Beecrowd/1094 - Experiencias/main.cpp
+++ Exercises-Beecrowd/1094 - Experiencias/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
-   int n ;
+   int n , cobaias;
    cin>> n;
 
    for( int i = 0; i < n ; i++){
