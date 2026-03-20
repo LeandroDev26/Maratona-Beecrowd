a42639be357e8fe5c3b3e55d91a1a96c3ee8ce4d
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-20 15:41:29 -0300

    feat

--- Exercises-Beecrowd/1096 - Sequencia IJ 2/main.cpp
+++ Exercises-Beecrowd/1096 - Sequencia IJ 2/main.cpp
@@ -4,21 +4,22 @@ using namespace std;
 
 int main()
 {
-    int i = 1, j = 7, aux = 0 ,soma = 0;
+    int i = 1, j = 7, aux = 0,soma = 0;
 
-    for(int i = 1 ; i <=9 ; i++)
+    while()
     {
 
-        while( j >= 5 && i <= 9)
+
+        while( j >= 5 )
         {
             soma += i;
             cout<< "I=" << i <<  " J=" <<j <<endl;
             j --;
 
+
         }
+
     }
-
-
 
 
     return 0;
