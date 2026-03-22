af752b5eda44399a4b6807845028ada8f5fe7df6
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-22 09:35:48 -0300

    feat

--- Exercises-Beecrowd/1101 - Sequencia de Numeros e Soma/main.cpp
+++ Exercises-Beecrowd/1101 - Sequencia de Numeros e Soma/main.cpp
@@ -4,15 +4,19 @@ using namespace std;
 
 int main()
 {
-  int m , n , x = 7;
+    int m, n, x = 7;
 
 
-  while(x <= 0){
+    while(x <= 0)
+    {
 
-   cin>> m >> n;
+        cin>> m >> n;
 
-
-  }
+        if(m <= 0 || n <=0)
+        {
+            x = 0;
+        }
+    }
 
 
 
