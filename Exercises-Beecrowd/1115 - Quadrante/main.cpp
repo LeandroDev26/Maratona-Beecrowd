d68942271274e5da587d564d0c9a91caecf238e4
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-22 11:33:30 -0300

    feat

--- Exercises-Beecrowd/1115 - Quadrante/main.cpp
+++ Exercises-Beecrowd/1115 - Quadrante/main.cpp
@@ -5,27 +5,34 @@ using namespace std;
 int main()
 {
 
- int x , y ;
- while(1){
+    int x, y ;
+    while(1)
+    {
 
-    cin>> x >> y;
+        cin>> x >> y;
 
-    if(x == 0 || y == 0){
-        break;
-    }
-    else if( x > 0 && y > 0){
+        if(x == 0 || y == 0)
+        {
+            break;
+        }
+        else if( x > 0 && y > 0)
+        {
 
-        cout<<"primeiro"<<endl;
+            cout<<"primeiro"<<endl;
+        }
+        else if(x < 0 && y > 0)
+        {
+            cout<<"segundo"<<endl;
+        }
+        else if(x < 0 && y < 0)
+        {
+            cout<<"terceiro"<<endl;
+        }
+        else
+        {
+            cout<<"quarto"<<endl;
+        }
     }
-    else if(x < 0 && y > 0){
-        cout<<"segundo"<<endl;
-    }
-    else if(x < 0 && y < 0){
-        cout<<"terceiro"<<endl;
-    }else{
-       cout<<"quarto"<<endl;
-    }
- }
 
 
 
