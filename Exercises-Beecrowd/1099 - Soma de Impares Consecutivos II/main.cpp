20c50ed7c73f6452bb8e4e58814d5b790119e95a
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-21 22:31:26 -0300

    feat

--- Exercises-Beecrowd/1099 - Soma de Impares Consecutivos II/main.cpp
+++ Exercises-Beecrowd/1099 - Soma de Impares Consecutivos II/main.cpp
@@ -5,10 +5,26 @@ using namespace std;
 int main()
 {
 
-  int n;
+  int n , x , y , ini , fim , soma;
+
   cin>> n;
 
    for(int i = 0; i < n; i++){
+
+    cin>> x >> y ;
+
+    if( x > y)
+    {
+
+        ini = y;
+        fim = x;
+    }else
+     {
+
+    ini = x;
+    fim = y;
+
+     }
 
 
    }
