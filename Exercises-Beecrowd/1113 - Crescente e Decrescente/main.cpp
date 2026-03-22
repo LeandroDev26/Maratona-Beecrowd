1100144401b19d0b8eb3c134c0fa005ec235c200
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-22 10:58:30 -0300

    feat

--- Exercises-Beecrowd/1113 - Crescente e Decrescente/main.cpp
+++ Exercises-Beecrowd/1113 - Crescente e Decrescente/main.cpp
@@ -5,19 +5,23 @@ using namespace std;
 int main()
 {
 
-  int x , y;
+    int x, y;
 
 
-  while(1){
+    while(1)
+    {
+
+        x = 0, y = 0;
+
+        cin>> x >> y;
+
+        if( x == y)
+        {
+            break;
+        }
 
 
-    cin>> x >> y;
-    if( x == y){
-        break;
     }
-
-
-  }
 
 
 
