584e35f14746c77cdc0a7b78773e300d34729cdf
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-22 11:31:57 -0300

    feat

--- Exercises-Beecrowd/1115 - Quadrante/main.cpp
+++ Exercises-Beecrowd/1115 - Quadrante/main.cpp
@@ -14,7 +14,14 @@ int main()
         break;
     }
     else if( x > 0 && y > 0){
+
         cout<<"primeiro"<<endl;
+    }
+    else if(x < 0 && y > 0){
+        cout"segundo"<<endl;
+    }
+    else if(x < 0 && y < 0){
+        cout<<"terceiro"<<endl;
     }
  }
 
