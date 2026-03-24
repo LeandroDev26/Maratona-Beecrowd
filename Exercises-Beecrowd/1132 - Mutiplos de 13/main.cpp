409b1ecd09590790852c54d7a061bc401342e6ec
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-24 13:12:45 -0300

    feat

--- Exercises-Beecrowd/1132 - Mutiplos de 13/main.cpp
+++ Exercises-Beecrowd/1132 - Mutiplos de 13/main.cpp
@@ -5,16 +5,18 @@ using namespace std;
 int main()
 {
 
-  int x = 0 , y = 0 , soma = 0;
-  cin>> x >> y;
+    int x = 0, y = 0, soma = 0;
+    cin>> x >> y;
 
-  for( int i = x ; i <= y; i++){
-      if(i % 2 !=0 ){
-         soma+= i;
-      }
-  }
+    for( int i = x ; i <= y; i++)
+    {
+        if(i % 2 !=0 )
+        {
+            soma+= i;
+        }
+    }
 
-  cout<<soma<<endl;
+    cout<<soma<<endl;
 
 
 
