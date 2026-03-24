5ebf747ba44c276a0c33bb5b715ca9c149bd1a56
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-24 07:00:44 -0300

    feat

--- Exercises-Beecrowd/1118 - Varias Notas Com Validacao/main.cpp
+++ Exercises-Beecrowd/1118 - Varias Notas Com Validacao/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
 
+    double nota = 0, media = 0;
+    cout<< fixed << setprecision(2);
 
+    while (1)
+    {
+        cin>> nota;
 
+        if( nota >= 0 && nota<= 10)
+        {
+            media+= nota;
 
+        }
+        else
+        {
+            cout<<"nota invalida"<<endl;
+        }
 
+    }
 
-
-
+    media /= 2;
+    cout<<"media = "<<media<<endl;
 
 
 
