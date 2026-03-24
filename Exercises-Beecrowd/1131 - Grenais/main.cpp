bb7d942de5079d05559207fa20ff4e0476460a88
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-24 11:30:56 -0300

    feat

--- Exercises-Beecrowd/1131 - Grenais/main.cpp
+++ Exercises-Beecrowd/1131 - Grenais/main.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main()
 {
-int opc = 1 ;
+    int opc = 1 ;
+
+    while(opc == 1)
+    {
+
+
+
+     opc = 0;
+     while(opc != 1 && opc != 2){
+        cout<<"Novo grenal (1-sim 2-nao)"<< endl;
+     }
+
+    }
+
 
 
 
