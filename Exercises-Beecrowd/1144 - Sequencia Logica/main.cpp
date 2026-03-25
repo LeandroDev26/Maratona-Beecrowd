d430ada8cf57e2fb53728fb1b73f183adbaba68b
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-25 20:41:36 -0300

    feat

--- Exercises-Beecrowd/1144 - Sequencia Logica/main.cpp
+++ Exercises-Beecrowd/1144 - Sequencia Logica/main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main()
 {
-    int n , num = 1;
+    int n, num = 1;
     cin>> n ;
     n = n*2;
 
-    for(int i = 0 ; i < n; i++){
-
+    for(int i = 0 ; i < n; i++)
+    {
+        cout<< num <<" "<<(num * num)<< " " << (num* num * num)<<endl;
     }
 
 
