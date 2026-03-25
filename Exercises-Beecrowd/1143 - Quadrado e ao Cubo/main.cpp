80ace103aed292a37f30bcec53439bf746abd465
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-25 17:15:40 -0300

    feat

--- Exercises-Beecrowd/1143 - Quadrado e ao Cubo/main.cpp
+++ Exercises-Beecrowd/1143 - Quadrado e ao Cubo/main.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main()
 {
-  int n = 0 , numero = 1;
-  cin>> n;
+    int n = 0, numero = 1;
+    cin>> n;
 
-  for (int i = 0 ; i < n ; i++){
-       cout<< numero << " " << (numero*numero) << " " << (numero* numero * numero)<<endl;
-       numero++;
-  }
+    for (int i = 0 ; i < n ; i++)
+    {
+        cout<< numero << " " << (numero*numero) << " " << (numero* numero * numero)<<endl;
+        numero++;
+    }
 
 
 
