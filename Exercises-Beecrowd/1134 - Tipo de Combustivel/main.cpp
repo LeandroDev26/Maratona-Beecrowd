3657f0d8a0ced569355736d4ed5259d5a1d4ee7e
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-25 16:25:46 -0300

    feat

--- Exercises-Beecrowd/1134 - Tipo de Combustivel/main.cpp
+++ Exercises-Beecrowd/1134 - Tipo de Combustivel/main.cpp
@@ -4,15 +4,18 @@ using namespace std;
 
 int main()
 {
-    int opc = 0, alco = 0, gaso = 0 , dies = 0;
+    int opc = 0, alco = 0, gaso = 0, dies = 0;
 
     while(opc != 4)
     {
-       cin>> opc;
+        cin>> opc;
 
-       if(opc == 1){
+        if(opc == 1)
+        {
 
-       }
+            alco ++;
+
+        }
     }
 
 
