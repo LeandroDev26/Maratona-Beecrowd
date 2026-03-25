4779ae799a770e64d927a3ed3490cffe23b259b2
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-25 16:54:12 -0300

    feat

--- Exercises-Beecrowd/1142 - PUM/main.cpp
+++ Exercises-Beecrowd/1142 - PUM/main.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main()
 {
 
-    int n = 0 , numero = 1;
+    int n = 0, numero = 1;
     cin>> n ;
 
     for (int i = 0; i < n ; i++)
     {
+
+        cout<<i<< " " << i + 1 << " " << i+2 << " " << "PUM";
 
 
     }
