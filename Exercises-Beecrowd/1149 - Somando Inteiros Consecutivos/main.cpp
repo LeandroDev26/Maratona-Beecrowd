8ca45bad5e98c534f7b3b36eb3cc6b9dc914b6d3
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-26 16:15:29 -0300

    feat

--- Exercises-Beecrowd/1149 - Somando Inteiros Consecutivos/main.cpp
+++ Exercises-Beecrowd/1149 - Somando Inteiros Consecutivos/main.cpp
@@ -5,19 +5,23 @@ using namespace std;
 int main()
 {
 
-   int a = 0 , n = 0;
+    int a = 0, n = 0;
 
-   cin>> a >> n;
-   if(n <= 0){
-    while(n <= 0){
-        cin>> n;
+    cin>> a >> n;
+
+    if(n <= 0)
+    {
+        while(n <= 0)
+        {
+            cin>> n;
+        }
     }
-   }
 
-   for(int i = 0; i <= n; i ++){
+    for(int i = 0; i <= n; i ++)
+    {
 
 
-   }
+    }
 
 
 
