527257d039876efc92db7bde508dc68b4c41269d
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-26 15:25:14 -0300

    feat

--- Exercises-Beecrowd/1145 - Sequencia Logica 2/main.cpp
+++ Exercises-Beecrowd/1145 - Sequencia Logica 2/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 
-    int x, y, cont =0;
+    int x, y, cont = 0;
     cin>> x >> y;
     for(int i = 1; i <= y; i++)
     {
@@ -17,6 +17,7 @@ int main()
 
 
         }
+        cont = 0;
     }
 
 
