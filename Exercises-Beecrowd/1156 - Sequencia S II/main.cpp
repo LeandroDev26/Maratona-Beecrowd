918c8888b57824edf14578089ee30a9eeb359148
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-27 23:13:56 -0300

    feat

--- Exercises-Beecrowd/1156 - Sequencia S II/main.cpp
+++ Exercises-Beecrowd/1156 - Sequencia S II/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double s = 0;
+
+    for(int i = 1; i <= 100; i++ )
+    {
+         s += (1.0/i);
+    }
+     cout<< fixed << setprecision(2);
+     cout<< s <<endl;
+
+
     return 0;
 }
