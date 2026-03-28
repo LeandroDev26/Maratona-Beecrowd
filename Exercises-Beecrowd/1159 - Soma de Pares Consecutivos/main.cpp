97e9049e969c912fc2dd13e1b89e0a20d80ff910
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-28 20:52:45 -0300

    feat

--- Exercises-Beecrowd/1159 - Soma de Pares Consecutivos/main.cpp
+++ Exercises-Beecrowd/1159 - Soma de Pares Consecutivos/main.cpp
@@ -4,22 +4,29 @@ using namespace std;
 
 int main()
 {
-    int x = 0 , soma = 0 , cont = 0;
+    int x = 0, soma = 0, cont = 0;
     cin>> x;
 
     while( x != 0)
     {
 
-
+      soma = 0, cont = 0;
         if( x % 2 == 0)
         {
-          while(cont < 5){
-             soma += x;
-             x+=2;
-             cont++;
-          }
+            while(cont < 5)
+            {
+                soma += x;
+                x+=2;
+                cont++;
+            }
+
+        }else
+        {
+
 
         }
+
+        cout<< soma <<endl;
 
 
         cin>> x;
