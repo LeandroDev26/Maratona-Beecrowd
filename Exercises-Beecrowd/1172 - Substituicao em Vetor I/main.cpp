158bd2b8c97cc486495ebc3e3b0988ebba3ee313
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-29 10:54:02 -0300

    feat

--- Exercises-Beecrowd/1172 - Substituicao em Vetor I/main.cpp
+++ Exercises-Beecrowd/1172 - Substituicao em Vetor I/main.cpp
@@ -5,11 +5,22 @@ using namespace std;
 int main()
 {
 
-   vector <int> num ;
+    vector <int> num ;
+    int x = 0;
+    for(int i = 0 ; i < 10; i++)
+    {
+        cin>> x;
+        if( x <= 0){
+            x = 1;
+        }
+        num.push_back(x);
 
-   for(int i = 0 ; i < 10; i++){
+    }
 
-   }
+    for(int k = 0 ; k < num.size(); k++)
+    {
+        cout<<"X["<< k <<"]"<< " = " << num[k]<<endl;
+    }
 
 
 
