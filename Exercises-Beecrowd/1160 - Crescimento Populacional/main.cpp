5f3e68740cafd0d794e9230d63a6f7bb68be7bda
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-29 10:08:26 -0300

    feat

--- Exercises-Beecrowd/1160 - Crescimento Populacional/main.cpp
+++ Exercises-Beecrowd/1160 - Crescimento Populacional/main.cpp
@@ -5,12 +5,13 @@ using namespace std;
 int main()
 {
 
-  int t = 0 , pa = 0, pb = 0;
+  int t = 0 , pa = 0, pb = 0 , anos = 0;
   double g1 = 0.0 , g2 = 0.0;
   cin>> t ;
 
   for( int i = 0 ; i < t ; i++){
 
+        cin>> pa >> pb >> g1 >> g2;
   }
 
 
