a0a90c9caccb321525258309382eef9bff3053c0
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-28 21:12:44 -0300

    feat

--- Exercises-Beecrowd/1164 - Numero Perfeito/main.cpp
+++ Exercises-Beecrowd/1164 - Numero Perfeito/main.cpp
@@ -4,13 +4,20 @@ using namespace std;
 
 int main()
 {
-    int n , x;
+    int n , x , soma = 0;
 
     cin>> n;
 
     for(int i = 0; i < n ; i++){
 
          cin>> x;
+
+        for(int k = 1; k < x; k++){
+
+             if( k % x == 0 ){
+
+             }
+        }
 
     }
 
