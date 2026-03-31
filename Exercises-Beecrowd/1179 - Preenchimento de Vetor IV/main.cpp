6560233ca6ece991b5701214ae2c3e0adf59c9e9
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-31 14:11:28 -0300

    feat

--- Exercises-Beecrowd/1179 - Preenchimento de Vetor IV/main.cpp
+++ Exercises-Beecrowd/1179 - Preenchimento de Vetor IV/main.cpp
@@ -39,7 +39,7 @@ int main()
                 {
                     cout<< "impar[" << k << "]" << " = " << impar[k] << endl;
                 }
-
+                impar.clear();
                 contimp = 0;
             }
         }
