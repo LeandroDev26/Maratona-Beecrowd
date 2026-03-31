2a73592001427b3d4d2ca96df26adfb795c0e88b
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-30 23:36:59 -0300

    feat

--- Exercises-Beecrowd/1176 - Fibonacci em Vetor/main.cpp
+++ Exercises-Beecrowd/1176 - Fibonacci em Vetor/main.cpp
@@ -7,37 +7,38 @@ int main()
 {
     int t, n, a = 0, b = 1, aux = 0;
     vector<int> fibonacci;
+
+    for ( int k = 0 ; k < n; k++)
+    {
+
+        if( k == n-1)
+        {
+            fibonacci.push_back(a);
+        }
+        else
+        {
+            aux = a;
+            a = b;
+            b += aux;
+        }
+    }
+
+
     cin>> t;
 
     for(int i = 0; i < t; i++)
     {
         cin>> n;
 
-        for ( int k = 0 ; k < n; k++)
-        {
 
-            if( k == n-1)
-            {
-                fibonacci.push_back(a);
-            }
-            else
-            {
-                aux = a;
-                a = b;
-                b += aux;
-            }
-        }
-
-
-        for(int j = 0 ; j < fibonacci.size(); j++)
-        {
-            cout<<"Fib(" << j << ")"<< " = " << fibonacci[j]<<endl;
-        }
 
     }
 
 
-
+    for(int j = 0 ; j < fibonacci.size(); j++)
+    {
+        cout<<"Fib(" << j << ")"<< " = " << fibonacci[j]<<endl;
+    }
 
 
 
