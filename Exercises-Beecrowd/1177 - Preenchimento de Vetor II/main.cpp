fb14eb0d249216080930d75d7f15eaf56322ff52
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-30 21:08:27 -0300

    feat

--- Exercises-Beecrowd/1177 - Preenchimento de Vetor II/main.cpp
+++ Exercises-Beecrowd/1177 - Preenchimento de Vetor II/main.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 int main()
 {
-    int t;
-     vector <int> n;
+    int t , cont = 0;
+    vector <int> n;
 
-     cin>> t;
+    cin>> t;
 
-    for(int i = 0; i < 33; i++)
+    while(cont < 100)
     {
-
         for(int k = 0; k <= t -1; k++)
         {
-           n.push_back(k);
+            n.push_back(k);
         }
     }
-       for(int j = 0 ; j < n.size(); j++){
-            cout<<"N[" << j << "]" <<" = "<< n[j] <<endl;
-       }
+    for(int j = 0 ; j < n.size(); j++)
+    {
+        cout<<"N[" << j << "]" <<" = "<< n[j] <<endl;
+    }
 
 
 
