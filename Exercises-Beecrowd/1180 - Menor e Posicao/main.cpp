4376aeab0a66c7c796af4b3edda0b11ebd863ec4
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-31 19:18:53 -0300

    feat

--- Exercises-Beecrowd/1180 - Menor e Posicao/main.cpp
+++ Exercises-Beecrowd/1180 - Menor e Posicao/main.cpp
@@ -6,16 +6,20 @@ using namespace std;
 int main()
 {
 
-    int n , y;
+    int n, y;
     vector <int> x;
     cin>> n;
 
     for(int i = 0 ; i < n ; i++)
     {
-          cin>> y;
-          x.push_back(y);
+        cin>> y;
+        x.push_back(y);
     }
 
+    for(int k = 0 ; k < x.size(); k++)
+    {
+
+    }
 
 
 
