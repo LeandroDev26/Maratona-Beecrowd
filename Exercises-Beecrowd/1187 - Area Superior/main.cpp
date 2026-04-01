8d43369aae9dfd3ef0be45739f9b344bd8a4523c
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-04-01 20:00:27 -0300

    feat

---
+++ Exercises-Beecrowd/1187 - Area Superior/main.cpp
@@ -0,0 +1,20 @@
+##include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+    char t;
+    double soma = 0;
+    int linhas = 12;
+    int colunas = 12;
+    vector<vector<double>> matriz(linhas, vector<double>(colunas, 0));
+
+    cin>> t;
+
+
+    return 0;
+}
