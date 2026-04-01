69f3a6be3b3c696da3cdb592ddb789116e6c33b2
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-31 22:57:24 -0300

    feat

--- Exercises-Beecrowd/1181 - Linha na Matriz/main.cpp
+++ Exercises-Beecrowd/1181 - Linha na Matriz/main.cpp
@@ -8,8 +8,19 @@ int main()
 
     int linhas = 12;
     int colunas = 12;
-    vector<vector<int>> matriz(linhas, vector<int>(colunas , 0));
+    vector<vector<int>> matriz(linhas, vector<int>(colunas, 0));
 
+
+    // O laço de fora (i) controla os andares (linhas)
+    for(int i = 0; i < linhas; i++)
+    {
+
+        // O laço de dentro (j) anda pelas gavetas (colunas) daquele andar
+        for(int j = 0; j < colunas; j++)
+        {
+            cin >> matriz[i][j]; // Lendo e guardando o valor na coordenada exata
+        }
+    }
 
 
 
