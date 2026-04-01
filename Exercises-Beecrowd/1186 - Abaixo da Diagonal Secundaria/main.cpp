4b53f1307467937158d1009e617f16b0af9f931b
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-04-01 19:42:39 -0300

    feat

--- Exercises-Beecrowd/1186 - Abaixo da Diagonal Secundaria/main.cpp
+++ Exercises-Beecrowd/1186 - Abaixo da Diagonal Secundaria/main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include <iomanip>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    char t;
+    double soma = 0;
+    int linhas = 12;
+    int colunas = 12;
+    vector<vector<double>> matriz(linhas, vector<double>(colunas, 0));
+
+    cin>> t;
+
+
+    for(int i = 0; i < linhas; i++)
+    {
+
+        for(int j = 0; j < colunas; j++)
+        {
+            cin >> matriz[i][j];
+        }
+    }
+
+
+    cout<< fixed << setprecision(1);
+
+    for(int l = 0; l < 11; l++)
+    {
+        for(int k = 0; k < 11 - l; k++)
+        {
+            soma += matriz[l][k];
+        }
+    }
+    if( t == 'S')
+    {
+        cout<< soma <<endl;
+    }
+    else if( t == 'M')
+    {
+
+        cout << soma / 66.0 << endl;
+    }
+
+
+
+
+
+
+
     return 0;
 }
