65495bfc84c2c617947a9742740f9e88d232b632
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-04-02 19:40:08 -0300

    feat

--- Exercises-Beecrowd/1189 - Area Esquerda/main.cpp
+++ Exercises-Beecrowd/1189 - Area Esquerda/main.cpp
@@ -19,7 +19,7 @@ int main()
     for(int i = 0 ; i < linhas; i++)
     {
 
-        for(int j = 0 ; i < colunas ; j++)
+        for(int j = 0 ; j < colunas ; j++)
         {
             cin>> matriz[i][j];
         }
