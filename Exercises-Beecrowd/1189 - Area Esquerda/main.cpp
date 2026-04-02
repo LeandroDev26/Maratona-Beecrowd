ccfd14b1b42c730cc719a144990beddb146340ce
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-04-02 19:25:18 -0300

    feat

--- Exercises-Beecrowd/1189 - Area Esquerda/main.cpp
+++ Exercises-Beecrowd/1189 - Area Esquerda/main.cpp
@@ -11,29 +11,33 @@ int main()
     double soma = 0;
     int linhas = 12;
     int colunas = 12;
-    vector< vector <double>> matriz (linhas , vector<double> (colunas , 0));
+    vector< vector <double>> matriz (linhas, vector<double> (colunas, 0));
 
 
-     cin>> t ;
+    cin>> t ;
 
-     for(int i = 0 ; i < linhas; i++){
+    for(int i = 0 ; i < linhas; i++)
+    {
 
-          for(int j = 0 ; i < colunas ; j++){
-              cin>> matriz[i][j];
-          }
-     }
+        for(int j = 0 ; i < colunas ; j++)
+        {
+            cin>> matriz[i][j];
+        }
+    }
 
 
-     for(int l = 0 ; l < linhas - 1; l++){
+    for(int l = 0 ; l < linhas - 1; l++)
+    {
 
-        for(int k = 0 ; k < linhas - 2; k++){
+        for(int k = 0 ; k < linhas - 2; k++)
+        {
 
             soma += matriz[l][k];
         }
 
-     }
+    }
 
-      if( t == 'S')
+    if( t == 'S')
     {
         cout<< soma <<endl;
     }
