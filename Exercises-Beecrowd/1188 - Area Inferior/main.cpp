d1e25a33fda5559bd565fc83aa45d41302aa63df
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-04-01 23:07:34 -0300

    feat

--- Exercises-Beecrowd/1188 - Area Inferior/main.cpp
+++ Exercises-Beecrowd/1188 - Area Inferior/main.cpp
@@ -7,6 +7,11 @@ using namespace std;
 int main()
 {
 
+ char t;
+    double soma = 0;
+    int linhas = 12;
+    int colunas = 12;
+    vector<vector<double>> matriz(linhas, vector<double>(colunas, 0));
 
 
 
