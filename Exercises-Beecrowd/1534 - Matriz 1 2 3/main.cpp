edf6e3312b43f8a3841253ec884e6d2ebca876fd
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-04 20:47:44 -0300

    feat

--- Exercises-Beecrowd/1534 - Matriz 1 2 3/main.cpp
+++ Exercises-Beecrowd/1534 - Matriz 1 2 3/main.cpp
@@ -7,11 +7,22 @@ int main()
 {
 
     int n, linhas, colunas;
+
+    while(cin>> n){
     cin>> n ;
+    linhas = n;
+    colunas = n;
     vector< vector <int>> matriz (linhas, vector<int> (colunas, 0));
 
+    for(int i = 0; i < linhas; i++){
 
+        for(int j = 0; j < colunas; j++){
 
+           cout<< matriz[i][j]<<endl;
+        }
+    }
+
+    }
 
 
 
