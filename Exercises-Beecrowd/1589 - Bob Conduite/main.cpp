43596029a8eee9be06116e78dde2c1fe38a0ed4b
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-13 20:41:57 -0300

    feat

--- Exercises-Beecrowd/1589 - Bob Conduite/main.cpp
+++ Exercises-Beecrowd/1589 - Bob Conduite/main.cpp
@@ -5,9 +5,12 @@ using namespace std;
 int main()
 {
 
-    int t ;
+    int t , r1 , r2 ;
     cin>> t ;
-    while()
+    for( int i = 0; i < t ; i++)
+    {
+         //cin>>
+    }
 
 
 
@@ -15,5 +18,5 @@ int main()
 
 
 
-        return 0;
+    return 0;
 }
