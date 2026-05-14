aa28e141e9f5bb46382d2f58c78b4df5752aac8c
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-14 20:37:51 -0300

    feat

--- Exercises-Beecrowd/1827 - Matriz Quadrada IV/main.cpp
+++ Exercises-Beecrowd/1827 - Matriz Quadrada IV/main.cpp
@@ -9,11 +9,13 @@ int main()
 
     while(cin>> n)
     {
-
         for(int i = 0 ; i < n; i++)
         {
+           int cont = 0;
+
             for(int j = 0 ; j < n; j++)
             {
+
                 if ( i == n/2 && j == n/2)
                 {
                     cout<<"4";
@@ -25,7 +27,15 @@ int main()
                 }
                 else if( i == j )
                 {
+                    cout<<"2";
+                }
+                else if(i + j == n -1)
+                {
                     cout<<"3";
+                }
+                else
+                {
+                    cout<<"0";
                 }
             }
         }
