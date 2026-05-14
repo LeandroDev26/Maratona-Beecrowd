328d045057da898021bb432dae99df77c9e3c882
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-14 19:05:24 -0300

    feat

--- Exercises-Beecrowd/1759 - Ho Ho Ho/main.cpp
+++ Exercises-Beecrowd/1759 - Ho Ho Ho/main.cpp
@@ -6,13 +6,18 @@ using namespace std;
 int main()
 {
 
-    int n ;
+    int n , cont = 0;
 
     cin>> n;
 
     for(int i = 0 ; i < n; i++)
     {
        cout<< "Ho ";
+
+       cont++;
+       if (cont == (n-1)){
+
+       }
     }
 
 
