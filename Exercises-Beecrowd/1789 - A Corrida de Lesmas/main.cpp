ea44eaa81c5c7aabad3de0599f07a8b7e2d48ff4
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-14 19:45:58 -0300

    feat

--- Exercises-Beecrowd/1789 - A Corrida de Lesmas/main.cpp
+++ Exercises-Beecrowd/1789 - A Corrida de Lesmas/main.cpp
@@ -6,13 +6,14 @@ using namespace std;
 int main()
 {
 
-
+     vector <int> lesmas;
     int t, l , maior = 0;
     while(cin>> t)
     {
         for(int i = 0 ; i < t; i++)
         {
             cin>> l;
+
 
 
 
