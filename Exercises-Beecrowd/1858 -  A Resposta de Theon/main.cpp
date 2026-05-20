2fe1c0c3ff35d7fe2574a763074ae2dc9c0d03d9
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-20 20:47:38 -0300

    feat

--- Exercises-Beecrowd/1858 -  A Resposta de Theon/main.cpp
+++ Exercises-Beecrowd/1858 -  A Resposta de Theon/main.cpp
@@ -16,12 +16,12 @@ int main()
         if(t < menor)
         {
             menor = t ;
-            posicao = i + 1;
+            //posicao = i + 1;
         }
-        cout<< posicao <<endl;
+
     }
 
-
+    cout<< menor <<endl;
 
 
     return 0;
