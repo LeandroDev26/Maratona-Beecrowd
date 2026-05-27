328fcb791f7bda1fb4000a2902ffaddc09d2b99e
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-27 20:35:20 -0300

    feat

--- Exercises-Beecrowd/1864 - Nossos Dias Nunca Voltarao/main.cpp
+++ Exercises-Beecrowd/1864 - Nossos Dias Nunca Voltarao/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
 int main()
@@ -7,7 +7,6 @@ int main()
 
     int n ;
     cin>> n;
-
     for (int i = 0; i < n; i++)
     {
     }
