bd6bb4e82036099295a1faaa7405ceeec00ab488
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-26 23:31:37 -0300

    feat

--- Exercises-Beecrowd/1848 - Corvo Contador/main.cpp
+++ Exercises-Beecrowd/1848 - Corvo Contador/main.cpp
@@ -20,12 +20,18 @@ int main()
         else if( corvo == "--*")
         {
             soma += 1;
-        } else if( corvo == "-*-")
+        }
+        else if( corvo == "-*-")
         {
             soma += 2;
-        } else if( corvo == "-**")
+        }
+        else if( corvo == "-**")
         {
             soma += 3;
+        }
+        else if( corvo == "*--")
+        {
+            soma += 4;
         }
     }
 
