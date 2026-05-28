275620354b62dddb63fe489bcf9321716f3fceee
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-28 20:07:08 -0300

    feat

--- Exercises-Beecrowd/1914 - De Quem eh a Vez/main.cpp
+++ Exercises-Beecrowd/1914 - De Quem eh a Vez/main.cpp
@@ -6,14 +6,16 @@ using namespace std;
 int main()
 {
 
-    int qt, n, m , soma = 0;
+    int qt, n, m , soma ;
     string nome1, opc1, nome2, opc2;
     cin>> qt;
 
     for(int i = 0 ; i < qt ; i++)
     {
+        soma = 0;
         cin>> nome1 >> opc1 >> nome2 >> opc2;
         cin>> n >> m;
+        soma = (n + m);
 
 
     }
