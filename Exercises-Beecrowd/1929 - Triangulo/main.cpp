807b34a94c77afad421df46fa39feac3b6b2c5bb
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-30 09:48:39 -0300

    feat

--- Exercises-Beecrowd/1929 - Triangulo/main.cpp
+++ Exercises-Beecrowd/1929 - Triangulo/main.cpp
@@ -5,111 +5,12 @@ using namespace std;
 int main()
 {
 
-    int a, b, c, d, maiorAb, maiorAc , maiorAd ,maiorCd , maiorBc , maiorBd;
+    int a, b, c, d;
 
     cin>> a >> b >> c >> d;
 
-        if( a > b )
-        {
-            maiorAb = a;
-
-        }
-        else
-        {
-            maiorAb = b;
-
-        }
-
-          if( a > c )
-        {
-            maiorAc = a;
-
-        }
-        else
-        {
-            maiorAc = c;
-
-        }
-
-          if( a > d )
-        {
-            maiorAd = a;
-
-        }
-        else
-        {
-            maiorAd = d;
-
-        }
-
-        if( b > c )
-        {
-            maiorBc = b;
-
-        }
-        else
-        {
-            maiorBc = c;
-
-        }
-
-         if( b > d )
-        {
-            maiorBd = b ;
-
-        }
-        else
-        {
-            maiorBd= d;
-
-        }
-
-        if( c > d )
-        {
-            maiorCd = c;
-
-        }
-        else
-        {
-            maiorCd = d;
-
-        }
-
-
-    if (a + b > maiorCd)
-    {
-        cout << "S" << endl;
-    }
-    else if( a + c > maiorBd)
-    {
-        cout << "S" << endl;
-
-    }
-    else if( a + d > maiorBc)
-    {
-        cout << "S" << endl;
-
-    }
-    else if( (b + c  > a) && (b + c > d))
-    {
-        cout << "S" << endl;
-
-    }
-    else if( (b + d > a) && (b + d > c))
-    {
-        cout << "S" << endl;
-
-    }
-    else if( (c + d > a) && (c + d > b))
-    {
-        cout << "S" << endl;
-
-    }
-    else
-    {
-        cout<< "N" <<endl;
-    }
-
-
+     if ( a + b > c && a + c > d && b + c > a){
+        cout<<"S" <<endl;
+     }
     return 0;
 }
