f391964f2bedafc7e283922aec7f8cc5c7388408
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-30 14:28:03 -0300

    feat

--- Exercises-Beecrowd/1957 - Converter para Hexadecimal/main.cpp
+++ Exercises-Beecrowd/1957 - Converter para Hexadecimal/main.cpp
@@ -6,47 +6,58 @@ using namespace std;
 int main()
 {
 
-    int v , quociente = 0 , resto = 0;
+    int v, quociente = 0, resto = 0;
     string num;
-                                                                /*10 = A
-                                                                11 = B
-                                                                12 = C
-                                                                13 = D
-                                                                14 = E
-                                                                15 = F
-                                                                 */
+    /*10 = A
+    / A mágica acontece nesta única linha:
+    cout << uppercase << hex << v << endl;  11 = B
+    12 = C
+    13 = D
+    14 = E
+    15 = F
+     */
     cin>> v;
 
 
-    while(v > 0){
+    while(v > 0)
+    {
 
-          quociente = v / 16;
-          resto = v % 16;
-          if( resto >= 10 && resto <= 15){
+        quociente = v / 16;
+        resto = v % 16;
+        if( resto >= 10 && resto <= 15)
+        {
 
-              if( resto == 10){
+            if( resto == 10)
+            {
                 num = "A" + num ;
-              }else
-              if( resto == 11){
+            }
+            else if( resto == 11)
+            {
                 num = "B" + num ;
-              }else
-              if( resto == 12  ){
+            }
+            else if( resto == 12  )
+            {
                 num = "C" + num ;
-              }else
-              if( resto == 13 ){
+            }
+            else if( resto == 13 )
+            {
                 num = "D" + num ;
-              }else
-              if( resto == 14 ){
+            }
+            else if( resto == 14 )
+            {
                 num = "E" + num ;
-              }else
-              if( resto == 15 ){
+            }
+            else if( resto == 15 )
+            {
                 num = "F" + num ;
-              }
-          }else{
-               num +=  to_string(resto);
-              }
+            }
+        }
+        else
+        {
+            num =  to_string(resto) + num;
+        }
 
-           v = quociente;
+        v = quociente;
 
 
 
