f8335a8cf406a731794551498c32683bb5a27d31
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-31 12:47:28 -0300

    feat

--- Exercises-Beecrowd/1960 - Numeracao Romana para Numeros de Pagina/main.cpp
+++ Exercises-Beecrowd/1960 - Numeracao Romana para Numeros de Pagina/main.cpp
@@ -9,6 +9,15 @@ int main()
     int n ;
     cin>> n;
 
+    /*
+     1, V é 5, X é 10, L é 50, C é 100, D é 500 e M representa 1000.
+     */
+
+     if( n - 1000 >= 0 ){
+        cout<< "M";
+        n -= 1000;
+     }
+
 
     return 0;
 }
