3495f90cc7530e35458c9390b39405a343f4d639
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-31 14:55:01 -0300

    feat

--- Exercises-Beecrowd/1961 - Pula Sapo/main.cpp
+++ Exercises-Beecrowd/1961 - Pula Sapo/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
 
-    int p, n;
-
+    int p, n , x;
+    vector<int> altura;
     cin>> p >> n;
 
     for (int i = 0 ; i < n; i++){
+         cin>> x ;
+         altura.push_back(x);
 
     }
 
