b8872e59938b306f255444c21507b3f61a50c291
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-05-31 15:31:23 -0300

    feat

--- Exercises-Beecrowd/1961 - Pula Sapo/main.cpp
+++ Exercises-Beecrowd/1961 - Pula Sapo/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -20,16 +21,17 @@ int main()
     for( int j = 0 ; j < altura.size(); j++)
     {
 
-        diferenca = p - altura[j];
+        diferenca = abs(altura[j] - altura[j+1]);
 
-        if(diferenca > p)
+        if(diferenca <= p )
         {
-            cout<< "GAME OVER" <<endl; // perdeu
-            return 0;
+            ok++;
+
         }
         else
         {
-            ok++;
+          cout<< "GAME OVER" <<endl; // perdeu
+         return 0;
         }
 
         if(ok == (n -1))
@@ -37,10 +39,6 @@ int main()
             cout<< " YOU WIN"<<endl;  //ganhou
         }
     }
-
-
-
-
 
 
     return 0;
