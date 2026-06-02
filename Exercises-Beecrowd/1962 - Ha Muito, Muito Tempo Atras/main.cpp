9e0606ac4d7f4da7bf6ef901d59b95108eb9403d
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-01 23:07:01 -0300

    feat

--- Exercises-Beecrowd/1962 - Ha Muito, Muito Tempo Atras/main.cpp
+++ Exercises-Beecrowd/1962 - Ha Muito, Muito Tempo Atras/main.cpp
@@ -5,12 +5,15 @@ using namespace std;
 int main()
 {
 
-    long long n , t ;
+    long long n, t ;
     cin>> n;
     for(int i = 0 ; i < n ; i++)
     {
         cin>> t ;
-
+        if((2015 - t) > 0 )
+        {
+            cout<< ( 2015 - t)<<endl;
+        }
     }
 
 
