4b7c964224b4486492a6d53a2b66b9fa799ef7d0
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-02 13:59:22 -0300

    feat

--- Exercises-Beecrowd/1973 - Jornada nas Estrelas/main.cpp
+++ Exercises-Beecrowd/1973 - Jornada nas Estrelas/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
 
-    int n ;
+    long long n, x ;
+    vector<long long> qt;
     cin>> n;
-
     for(int i = 0 ; i < n ; i++)
     {
-
+        cin>> x;
+        qt.push_back(x);
     }
 
 
