040fc7169d78204e14b6f53500d65433ca3cae75
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-02 14:46:40 -0300

    feat

--- Exercises-Beecrowd/1973 - Jornada nas Estrelas/main.cpp
+++ Exercises-Beecrowd/1973 - Jornada nas Estrelas/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main()
 {
-     int posicaoAtual = 0, posicaoMax = 0;
-    long long n, x, somaArray = 0 , brothercrazy = 0;
+    int posicaoAtual = 0, posicaoMax = 0;
+    long long n, x, somaArray = 0, brothercrazy = 0;
     vector<long long> qt;
     cin>> n;
     for(int i = 0 ; i < n ; i++)
@@ -16,21 +16,31 @@ int main()
         qt.push_back(x);
     }
 
-  while(posicaoAtual >= 0 && posicaoAtual < qt.size() ){
+    while(posicaoAtual >= 0 && posicaoAtual < qt.size() )
+    {
 
-       if(posicaoAtual > posicaoMax){
-          posicaoMax = posicaoAtual;
-       }
+        if(posicaoAtual > posicaoMax)
+        {
+            posicaoMax = posicaoAtual;
+        }
 
-     if( qt[posicaoAtual] % 2 == 0){
-        //par
-     }else{
-     //impar
-     }
-  }
+        if( qt[posicaoAtual] % 2 == 0)
+        {
+            //par
+        }
+        else
+        {
+            //impar
+        }
+
+        if(qt[posicaoAtual] > 0)
+        {
+            qt[posicaoAtual] --;
+        }
+    }
 
 
 
 
-        return 0;
+    return 0;
 }
