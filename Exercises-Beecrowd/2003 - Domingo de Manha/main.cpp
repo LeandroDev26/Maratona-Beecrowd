cea3ef104df90ea8171ea21ba07e87e754ad6101
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-03 19:32:32 -0300

    feat

--- Exercises-Beecrowd/2003 - Domingo de Manha/main.cpp
+++ Exercises-Beecrowd/2003 - Domingo de Manha/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
 
-    int h, m ;
+    int h, m ,diferenca;
     char lixo;
     while(cin>> h >> lixo >> m)
     {
-
+         diferenca =
     }
 
 
