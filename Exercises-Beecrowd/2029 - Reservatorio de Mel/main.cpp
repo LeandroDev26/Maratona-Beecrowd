62a2c695dd30d7039a80a2d5f2f6bcf27e973705
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-03 20:55:01 -0300

    feat

--- Exercises-Beecrowd/2029 - Reservatorio de Mel/main.cpp
+++ Exercises-Beecrowd/2029 - Reservatorio de Mel/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-    double v ,d , a = 0.0 , r = 0.0 ;
+    double v ,d , a = 0.0 , r = 0.0 , pi = 3.14;
 
     while(cin>> v >> d){
 
       r = d/2;
+      a =
 
 
 
