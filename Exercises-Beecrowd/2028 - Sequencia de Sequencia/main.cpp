c8f1cc0a0e8121fad055a2bb3bf8708bb6568139
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-04 08:54:25 -0300

    feat

--- Exercises-Beecrowd/2028 - Sequencia de Sequencia/main.cpp
+++ Exercises-Beecrowd/2028 - Sequencia de Sequencia/main.cpp
@@ -9,9 +9,13 @@ int main()
 
     while(cin>> n)
     {
-
+        cout<<"0";
         for (int i = 1 ; i < n ; i++)
         {
+            for(int j = 1 ; j >= n; j++)
+            {
+                cout<< n;
+            }
 
         }
 
