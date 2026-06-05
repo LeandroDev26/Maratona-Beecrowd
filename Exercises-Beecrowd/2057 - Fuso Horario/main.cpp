9f4d39593359e1c602e77f4183ad469533e96298
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-05 18:43:37 -0300

    feat

--- Exercises-Beecrowd/2057 - Fuso Horario/main.cpp
+++ Exercises-Beecrowd/2057 - Fuso Horario/main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main()
 {
 
-    int s, t, f;
-    // saida , tempo de viagem , fusuorário
+    int s, t, f , c;
+    // saida , tempo de viagem , fusuorário , chegada
+
+
 
 
 
