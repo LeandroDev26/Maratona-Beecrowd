3cb5345b92c8ea45f8213432ce92d0f188e0c492
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-05 18:13:47 -0300

    feat

--- Exercises-Beecrowd/2031 - Pedra, Papel, Ataque Aereo/main.cpp
+++ Exercises-Beecrowd/2031 - Pedra, Papel, Ataque Aereo/main.cpp
@@ -6,18 +6,28 @@ using namespace std;
 int main()
 {
 
-   int n ;
-   string opc1 , opc2;
+    int n ;
+    string opc1, opc2;
 
-   cin>> n;
-   for(int i = 0 ; i < n; i++){
+    cin>> n;
+    for(int i = 0 ; i < n; i++)
+    {
 
         cin>> opc1 >> opc2 ;
 
-        if(opc1 == "ataque" && opc2 == "pedra"){
-             cout<<"Jogador 1 venceu"<<endl;
+        if(opc1 == "ataque" && opc2 == "pedra")
+        {
+            cout<<"Jogador 1 venceu"<<endl;
         }
-   }
+        else  if(opc1 == "pedra" && opc2 == "ataque")
+        {
+            cout<<"Jogador 2 venceu"<<endl;
+        }
+        else if(opc1 == "ataque" && opc2 == "ataque")
+        {
+            cout<<"Aniquilacao mutua"<<endl;
+        }
+    }
 
 
 
