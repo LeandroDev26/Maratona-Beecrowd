c0ae6ff5b3a6110e8eb35b4fad23129b6fd900c1
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-06 21:17:07 -0300

    feat

--- Exercises-Beecrowd/2059 - Impar, Par ou Roubo/main.cpp
+++ Exercises-Beecrowd/2059 - Impar, Par ou Roubo/main.cpp
@@ -9,8 +9,13 @@ int main()
 
     cin>> p >> j1 >> j2 >> r >> a;
 
-    if( r == 1 && a == 0){
+    if( r == 1 && a == 1)
+    {
         cout<<"Jogador 2 ganha!"<<endl;
+    }
+    else  if( r == 1 && a == 0)
+    {
+        cout<<"Jogador 1 ganha!"<<endl;
     }
 
 
