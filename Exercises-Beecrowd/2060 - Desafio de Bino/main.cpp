5f094b443b7dfe436e94728dbc55310e5798b87e
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-07 16:56:52 -0300

    feat

--- Exercises-Beecrowd/2060 - Desafio de Bino/main.cpp
+++ Exercises-Beecrowd/2060 - Desafio de Bino/main.cpp
@@ -6,12 +6,17 @@ int main()
 {
 
 
-    int n, t ;
+    int n, t, mult2 = 0, mult3 = 0, mult4 = 0, mult5 = 0;
     cin>> n;
 
     for(int i = 0 ; i < n ; i++)
     {
         cin>> t;
+
+        if(t % 2 == 0){
+            mult2++;
+        }
+
     }
 
 
