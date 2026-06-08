ad214f6fccaefb8d4deb6da95c7ebd71fc886dc4
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-08 20:14:01 -0300

    feat

--- Exercises-Beecrowd/2126 - Procurando Subsequencias/main.cpp
+++ Exercises-Beecrowd/2126 - Procurando Subsequencias/main.cpp
@@ -6,14 +6,46 @@ using namespace std;
 int main()
 {
 
-    int caso = 1, quantidade = 0, posicao = 0 ;
+    int caso = 1, quantidade , posicao ;
     string n1, n2 ;
 
     while( cin>> n1 >> n2 )
     {
         cout<<"Caso #"<<caso<<":"<<endl;
 
+        quantidade = 0;
+        posicao = 0;
+        size_t ponto_de_partida = 0;
 
+
+        while (true)
+        {
+            size_t indice = n2.find(n1, ponto_de_partida);
+
+            if (indice == string::npos)
+            {
+                break;
+            }
+
+
+            quantidade++;
+            posicao = indice + 1;
+
+
+            ponto_de_partida = indice + 1;
+        }
+
+        if (quantidade == 0)
+        {
+            cout << "Nao existe subsequencia" << endl;
+        }
+        else
+        {
+            cout << "Qtd.Subsequencias: " << quantidade << endl;
+            cout << "Pos: " << posicao << endl;
+        }
+
+        cout << endl;
 
 
 
