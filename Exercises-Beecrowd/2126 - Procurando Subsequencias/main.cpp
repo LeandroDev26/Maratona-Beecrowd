074f8b7be776eda9ec76174ba7a7eaa0bb63a605
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-08 19:40:53 -0300

    feat

--- Exercises-Beecrowd/2126 - Procurando Subsequencias/main.cpp
+++ Exercises-Beecrowd/2126 - Procurando Subsequencias/main.cpp
@@ -6,12 +6,14 @@ using namespace std;
 int main()
 {
 
-    int caso = 1 , quantidade , posicao ;
+    int caso = 1, quantidade, posicao ;
     string n1, n2 ;
 
     while( cin>> n1 >> n2 )
     {
+        quantidade = 0, posicao = 0;
         cout<<"Caso #"<<caso<<":"<<endl;
+
 
 
         caso++;
