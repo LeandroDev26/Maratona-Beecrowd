af683e22bc18b4a486c714c3bc6b4fb0cc52ed4b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-09 15:50:59 -0300

    feat

--- Exercises-Beecrowd/2140 - Duas Notas/main.cpp
+++ Exercises-Beecrowd/2140 - Duas Notas/main.cpp
@@ -11,46 +11,9 @@ int main()
     {
         // As notas disponíveis são: 2, 5, 10, 20, 50 e 100.
 
-        qtd = 0;
-        troco = ( n - m);
-        while( troco >= 0 )
-        {
-            if(troco % 100 > 0)
-            {
-                troco -= 100;
-                qtd++;
-            }
-            else if(troco % 50 > 0)
-            {
-                troco -= 50;
-                qtd++;
+        qtd = 1;
+        troco = ( m - n);
 
-            }
-            else if(troco % 20 > 0)
-            {
-                troco -= 20;
-                qtd++;
-
-            }
-            else if(troco % 10 > 0)
-            {
-                troco -= 10;
-                qtd++;
-
-            }
-            else if(troco % 5 > 0)
-            {
-                troco -= 5;
-                qtd++;
-
-            }
-            else if(troco % 2 > 0)
-            {
-                troco -= 2;
-                qtd++;
-
-            }
-        }
 
         if(qtd == 2)
         {
