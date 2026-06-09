ed32f94df4bb192a4493751b20c7bfa7bcec8a71
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-09 19:10:45 -0300

    feat

--- Exercises-Beecrowd/2143 - A Volta do Radar/main.cpp
+++ Exercises-Beecrowd/2143 - A Volta do Radar/main.cpp
@@ -5,15 +5,21 @@ using namespace std;
 int main()
 {
 
-    int t, n , totp;
+    int t, n, totp;
 
     while (cin>>  t && t != 0)
     {
         for (int i = 0 ; i < t; i++)
         {
             cin>> n;
-            if(n % 2 == 1){
-                 totp = (n - 1) * 2 + 1;
+            if(n % 2 == 1)
+            {
+                totp = (n - 1) * 2 + 1;
+            }
+            else
+            {
+                totp = (n - 2) * 2 + 2;
+
             }
         }
     }
