331c0b6bc52833ead9d56b90980e6d83e07f155b
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-06-08 21:05:58 -0300

    feat

--- Exercises-Beecrowd/2139 - Natal de Pedrinho/main.cpp
+++ Exercises-Beecrowd/2139 - Natal de Pedrinho/main.cpp
@@ -4,15 +4,20 @@ using namespace std;
 
 int main()
 {
-    int meses[] = {31, 29 , 31, 30, 31 , 30 , 31 , 31 , 30, 31 , 30 ,31};
-    int m , d;
+    int meses[12] = {31, 29 , 31, 30, 31 , 30 , 31 , 31 , 30, 31 , 30 ,31};
+    int m , d , soma = 0;
 
-    while( cin>> m >> d)
+    for(int i = 0 ; i < 12; i++){
+      soma += meses[i];
+    }
+
+
+    /*while( cin>> m >> d)
     {
 
     }
 
-
+*/
 
 
 
