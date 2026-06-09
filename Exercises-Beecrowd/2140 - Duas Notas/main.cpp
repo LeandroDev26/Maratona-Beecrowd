46cc6b267b207e2d3baafd46f0c2cac88e991ca4
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-09 15:27:30 -0300

    feat

--- Exercises-Beecrowd/2140 - Duas Notas/main.cpp
+++ Exercises-Beecrowd/2140 - Duas Notas/main.cpp
@@ -5,13 +5,14 @@ using namespace std;
 int main()
 {
 
-  int n , m , qtd;
+  int n , m , qtd , troco = 0 ;
 
     while(cin>> n >> m && n != 0 && m != 0){
        // As notas disponíveis são: 2, 5, 10, 20, 50 e 100.
 
        qtd = 0;
-        if()
+    troco = ( n - m);
+
 
 
     }
