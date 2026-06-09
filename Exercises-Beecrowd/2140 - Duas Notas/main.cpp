2db65469d7b1168060f1e6bb2bed6b3e5620b2d3
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-09 16:13:43 -0300

    feat

--- Exercises-Beecrowd/2140 - Duas Notas/main.cpp
+++ Exercises-Beecrowd/2140 - Duas Notas/main.cpp
@@ -5,26 +5,33 @@ using namespace std;
 int main()
 {
 
-    int n, m, qtd, troco = 0 ;
+    int n, m, soma, troco = 0 ;
     int notas [6] = { 2, 5, 10, 20, 50, 100};
     while(cin>> n >> m && n != 0 && m != 0)
     {
         // As notas disponíveis são: 2, 5, 10, 20, 50 e 100.
 
-        qtd = 1;
+
+        bool possivel ;
+
         troco = ( m - n);
 
 
         for(int i = 0 ; i < 6; i++)
         {
+            notas[i];
 
             for(int j = 0 ; j < 6 ; j++)
             {
+                soma += notas[i] + notas[j];
+                if(soma == troco)
+                {
 
+                }
             }
         }
 
-        if(qtd == 2)
+        if(possivel == 1)
         {
             cout<< "possible"<<endl;
         }
