8ba2ba5511505010e05b68da548bb4142ca4c644
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-10 09:08:09 -0300

    feat

--- Exercises-Beecrowd/2152 - Pepe Ja Tirei a Vela/main.cpp
+++ Exercises-Beecrowd/2152 - Pepe Ja Tirei a Vela/main.cpp
@@ -5,15 +5,19 @@ using namespace std;
 int main()
 {
 
-    int n , h , m , o;
+    int n, h, m, o;
     cin>> n ;
     for(int i = 0 ; i < n ; i++)
     {
-          cin>> h >> m >> o;
+        cin>> h >> m >> o;
 
-          if( h == 0 && m <= 9){
-
-          }
+        if( h == 0 && m <= 9)
+        {
+            if( o == 0)
+            {
+                cout<<"0"<< h<< ":50 - A porta fechou!"<<endl;
+            }
+        }
     }
 
 
