9a72b1a1dfe2666048685746fcbaf1eda68b4183
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-10 08:34:55 -0300

    feat

--- Exercises-Beecrowd/2147 - Galopeira/main.cpp
+++ Exercises-Beecrowd/2147 - Galopeira/main.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int main()
 {
-
+     double tempo;
     string frase;
     int c ;
     cin>> c;
     for(int i = 0 ; i < c ; i++)
     {
+         cin>> frase ;
 
     }
 
