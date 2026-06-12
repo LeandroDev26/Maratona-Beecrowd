0bf9f5917b9edcfe0faf1e503788820cff4d2e32
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-12 08:51:39 -0300

    feat

--- Exercises-Beecrowd/2162 - Picos e Vales/main.cpp
+++ Exercises-Beecrowd/2162 - Picos e Vales/main.cpp
@@ -7,12 +7,17 @@ int main()
 {
     vector<int> numeros;
     int n, h ;
+    bool padrao_valido = true;
     cin>> n ;
     for(int i = 0 ; i < n ; i++)
     {
         cin>> h;
         numeros.push_back(h);
 
+    }
+
+    if(n = 2 && numeros[0] == numeros[1])
+    {
     }
 
 
