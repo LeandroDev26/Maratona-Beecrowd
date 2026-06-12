f82007cb8df84cf213196cbbd3edab7cdf8aba78
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-12 15:46:37 -0300

    feat

--- Exercises-Beecrowd/2163 - O Despertar da Forca/main.cpp
+++ Exercises-Beecrowd/2163 - O Despertar da Forca/main.cpp
@@ -15,6 +15,14 @@ int main()
 
     cin>> n >> m;
 
+    for(int i = 0 ; i < n; i++)
+    {
+        for(int j = 0 ; j < m; j++)
+        {
+            cout<< terreno[i][j];
+        }
+    }
+
 
 
 
