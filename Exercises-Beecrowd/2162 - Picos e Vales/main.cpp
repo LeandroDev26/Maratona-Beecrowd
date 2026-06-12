7c56d78511b26d7bc1e254c051213f2dcf75ca34
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-12 08:48:35 -0300

    feat

--- Exercises-Beecrowd/2162 - Picos e Vales/main.cpp
+++ Exercises-Beecrowd/2162 - Picos e Vales/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-
-
+    vector<int> numeros;
     int n, h ;
     cin>> n ;
     for(int i = 0 ; i < n ; i++)
     {
+        cin>> h;
+        numeros.push_back(h);
 
     }
+
 
 
 
