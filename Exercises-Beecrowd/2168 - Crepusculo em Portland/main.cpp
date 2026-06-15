e2777844360562422bc17b85069e462843b4ae8c
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-15 20:26:19 -0300

    feat

--- Exercises-Beecrowd/2168 - Crepusculo em Portland/main.cpp
+++ Exercises-Beecrowd/2168 - Crepusculo em Portland/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 
-    int n , cameras = 0;
+    int n ;
     cin>> n ;
     vector< vector <int>> matriz( n+1, vector<int>(n+1, 0));
     for(int i = 0; i <= n ; i++)
@@ -19,6 +19,7 @@ int main()
 
     for(int l = 0 ; l < n; l++)
     {
+        int cameras = 0;
         for(int k = 0; k < n ; k++)
         {
 
