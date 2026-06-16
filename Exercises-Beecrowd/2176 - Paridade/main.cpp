bd69756f7beb31c35b964d1fe2992bf89e32daf3
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-15 21:34:43 -0300

    feat

--- Exercises-Beecrowd/2176 - Paridade/main.cpp
+++ Exercises-Beecrowd/2176 - Paridade/main.cpp
@@ -5,13 +5,21 @@ using namespace std;
 
 int main()
 {
+    int par = 0, impar = 0;
+    string s ;
+    cin>> s;
 
-   string s ;
-   cin>> s;
-
-   for(int i = 0; i < s.length - 1; i++){
-
-   }
+    for(int i = 0; i < s.length(); i++)
+    {
+        if( s[i] == 1)
+        {
+            par++;
+        }
+        else
+        {
+            impar++;
+        }
+    }
 
 
 
