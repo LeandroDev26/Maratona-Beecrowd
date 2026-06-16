b7183d28252fcaf3f8a75c87e424e5836adc1cbe
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-15 21:16:43 -0300

    feat

--- Exercises-Beecrowd/2172 - Evento/main.cpp
+++ Exercises-Beecrowd/2172 - Evento/main.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main()
 {
 
-  long long x , m , e;
+    long long x, m, e;
+    while(cin>> x >> m &&(x != 0 && m != 0))
+    {
+       e = (x*m);
+       cout<< e <<endl;
+    }
 
 
 
