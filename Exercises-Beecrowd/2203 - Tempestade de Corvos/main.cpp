03ce17ec394256383aeba8ad246a5b95f1236522
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-16 09:18:47 -0300

    feat

--- Exercises-Beecrowd/2203 - Tempestade de Corvos/main.cpp
+++ Exercises-Beecrowd/2203 - Tempestade de Corvos/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    int xf, yf, xi, yi, vi, r1, r2 , D;
+    int xf, yf, xi, yi, vi, r1, r2 , Di , Am;
 
     while(cin>> xf >> yf >> xi >> yi >> vi >> r1 >> r2)
     {
-          D = (sqrt(pow(xi - xf, 2) + pow(yi - yf , 2) ) );
-          D += (vi * 1.5);
+          Di = (sqrt(pow(xi - xf, 2) + pow(yi - yf , 2) ) );
+          Di += (vi * 1.5);
     }
 
 
