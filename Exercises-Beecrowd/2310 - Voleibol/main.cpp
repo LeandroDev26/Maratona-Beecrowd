86056ebcd19674811feae6c0c0cc5cd40fe7aa84
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-18 20:09:31 -0300

    feat

--- Exercises-Beecrowd/2310 - Voleibol/main.cpp
+++ Exercises-Beecrowd/2310 - Voleibol/main.cpp
@@ -6,15 +6,19 @@ using namespace std;
 int main()
 {
 
-    double s, b, a, s1, b1, a1 , tots = 0 , totb = 0, tota = 0;
+    double s, b, a, s1, b1, a1, tots = 0, totb = 0, tota = 0;
     int n;
     string nome;
 
-    for(int i = 0 ; i < n ; i++){
+    for(int i = 0 ; i < n ; i++)
+    {
         cin>> s >> b >> a ;
         tots += s;
         totb += b;
         tota += a;
+
+        cin>> s1 >> b1 >> a1 ;
+
 
     }
 
