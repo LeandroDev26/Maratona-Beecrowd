865729b50f9be5bf9546eed1d4f205b9ed077105
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-23 19:26:04 -0300

    feat

--- Exercises-Beecrowd/2311 - Saltos Ornamentais/main.cpp
+++ Exercises-Beecrowd/2311 - Saltos Ornamentais/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+
 using namespace std;
 
 int main()
 {
-
+   cout<< fixed << setprecision(2);
     int n ;
     string nome;
-    double gd, n1, n2, n3, n4, n5, n6, n7;
+    double gd, n1, n2, n3, n4, n5, n6, n7 , maior , menor;
     cin>> n ;
     for(int i = 0 ; i < n ; i++)
     {
