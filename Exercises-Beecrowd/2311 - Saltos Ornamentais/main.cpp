160a3d7ac5475adf7b3904bf75064cdc258bd902
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-23 19:18:26 -0300

    feat

--- Exercises-Beecrowd/2311 - Saltos Ornamentais/main.cpp
+++ Exercises-Beecrowd/2311 - Saltos Ornamentais/main.cpp
@@ -7,9 +7,11 @@ int main()
 
     int n ;
     string nome;
+    double gd, n1, n2, n3, n4, n5, n6, n7;
     cin>> n ;
     for(int i = 0 ; i < n ; i++)
     {
+        cin>> nome >> gd >> n1 >> n2 >> n3 >> n4 >> n5 >> n6 >> n7;
 
     }
 
