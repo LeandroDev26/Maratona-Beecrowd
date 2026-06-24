b3653d90a15d553e59c913c29611db858a8a7b79
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-23 21:16:03 -0300

    feat

--- Exercises-Beecrowd/2313 - Qual Triangulo/main.cpp
+++ Exercises-Beecrowd/2313 - Qual Triangulo/main.cpp
@@ -10,9 +10,14 @@ int main()
 
     if ( a + b > c || a + c > b || b + c > a )
     {
-      if(a == b && b == c){
-        cout<<"Valido-Equilatero"<<endl;
-      }
+        if(a == b && b == c)
+        {
+            cout<<"Valido-Equilatero"<<endl;
+        }
+        else if( a == b || a == c || b == c)
+        {
+
+        }
     }
     else
     {
