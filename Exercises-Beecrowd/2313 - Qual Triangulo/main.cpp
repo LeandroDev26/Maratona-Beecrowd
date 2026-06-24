ecca2b3b79ec84fd047abd2f963fc606a7672fae
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-23 21:07:18 -0300

    feat

--- Exercises-Beecrowd/2313 - Qual Triangulo/main.cpp
+++ Exercises-Beecrowd/2313 - Qual Triangulo/main.cpp
@@ -5,8 +5,20 @@ using namespace std;
 int main()
 {
 
-   int a , b , c ;
-   cin>> a >> b >> c;
+    int a, b, c ;
+    cin>> a >> b >> c;
+
+    if ( a + b > c )
+    {
+
+    }
+    else   if ( a + c > b )
+    {
+
+    } else   if ( b + c > a )
+    {
+
+    }
 
 
 
