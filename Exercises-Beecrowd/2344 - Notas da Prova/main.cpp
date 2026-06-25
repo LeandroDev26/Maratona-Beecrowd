cd4866548aff5d7934024e3d8317528c0f9a9262
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-24 21:38:07 -0300

    feat

--- Exercises-Beecrowd/2344 - Notas da Prova/main.cpp
+++ Exercises-Beecrowd/2344 - Notas da Prova/main.cpp
@@ -4,37 +4,29 @@ using namespace std;
 
 int main()
 {
-
     int n ;
-    cin>> n;
+    cin >> n;
+
     if(n == 0)
     {
-        cout<<"E"<<endl;
+        cout << "E" << endl;
     }
-    else if(n > 0 && n <= 35)
+    else if(n <= 35)
     {
-        cout<<"D"<<endl;
-
-    }  else if(n > 35 && n <= 60)
-    {
-        cout<<"C"<<endl;
-
-    }  else if(n > 60 && n <= 85)
-    {
-        cout<<"B"<<endl;
-
-    }  else if(n > 85 && n <= 100)
-    {
-        cout<<"A"<<endl;
-
+        cout << "D" << endl;
     }
-
-
-
-
-
-
-
+    else if(n <= 60)
+    {
+        cout << "C" << endl;
+    }
+    else if(n <= 85)
+    {
+        cout << "B" << endl;
+    }
+    else
+    {
+        cout << "A" << endl;
+    }
 
     return 0;
 }
