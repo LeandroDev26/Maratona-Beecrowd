1958e1eda94a8fcd1701612459d2bb9855719a10
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-26 23:33:26 -0300

    feat

--- Exercises-Beecrowd/2486 - C Mais ou Menos/main.cpp
+++ Exercises-Beecrowd/2486 - C Mais ou Menos/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
-
+#include <string>
+#include <map>
 using namespace std;
 
 int main()
 {
 
-
+ // Mapeamento dos alimentos e suas respectivas quantidades de Vitamina C (em mg)
+    map<string, int> tabela_vitC = {
+        {"suco de laranja", 120},
+        {"morango fresco", 85},
+        {"mamao", 85},
+        {"goiaba vermelha", 70},
+        {"manga", 56},
+        {"laranja", 50},
+        {"brocolis", 34}
+    };
 
 
 
