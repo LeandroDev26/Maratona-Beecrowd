df904bba8e884717645def1188de2a77b6684c3f
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-06-30 22:49:12 -0300

    feat

--- Exercises-Beecrowd/2523 - A Mensagem de Will/main.cpp
+++ Exercises-Beecrowd/2523 - A Mensagem de Will/main.cpp
@@ -15,6 +15,10 @@ int main()
         for(int i = 0 ; i < n; i++)
         {
             cin>> indice;
+            if(alfabeto[indice] != '/0' )
+            {
+               cout<< alfabeto[indice];
+            }
         }
     }
 
