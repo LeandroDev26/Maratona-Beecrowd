7a762e42e8389f0279b074bfdb59c6e789bcfc31
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-04 23:00:11 -0300

    feat

--- Exercises-Beecrowd/2547  - Montanha-Russa/main.cpp
+++ Exercises-Beecrowd/2547  - Montanha-Russa/main.cpp
@@ -5,12 +5,13 @@ using namespace std;
 int main()
 {
 
-  int n , amin , amax , avis;
+  int n , amin , amax , avis , tot;
 
   while(cin>> n >> amin >> amax){
 
+         tot = 0;
         for(int i = 0 ; i < n ; i++){
-
+            cin>> avis;
         }
   }
 
