e22062dc3c5a7ff629b117954761cc92a0c5ddb8
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-06 16:41:12 -0300

    feat

--- Exercises-Beecrowd/2502 - Decifrando a Carta Criptografada/main.cpp
+++ Exercises-Beecrowd/2502 - Decifrando a Carta Criptografada/main.cpp
@@ -1,29 +1,51 @@
 #include <iostream>
+#include <map>
 #include <string>
-#include <cctype>    // Biblioteca para tolower
-#include <algorithm>
+#include <cctype>
+
 using namespace std;
 
 int main()
 {
+    int c, n;
+    string linha1, linha2, frase;
+    map<char, char> dicionario;
 
 
-    int n, c ;
-    string cifra1, cifra2, frase;
-    cin>> n >> c;
-
-
-
-    for(int i = 0 ; i < n ; i++)
+    while (cin >> c >> n)
     {
 
+        cin.ignore();
+
+
+        getline(cin, linha1);
+        getline(cin, linha2);
+
+        for (int i = 0; i < c; i++)
+        {
+            char char1 = tolower(linha1[i]);
+            char char2 = tolower(linha2[i]);
+
+            dicionario[char1] = char2; // Z minúsculo aponta para P minúsculo
+            dicionario[char2] = char1; // P minúsculo aponta para Z minúsculo
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            getline(cin, frase);
+
+            // AQUI ENTRA A LÓGICA DE DECIFRAR E IMPRIMIR
+            // Você vai fazer um for para percorrer a string 'frase',
+            // checar no dicionario e imprimir a letra certa.
+
+            cout << endl; // Quebra de linha após imprimir a frase decifrada
+        }
+
+        cout << endl;
+
+
+        dicionario.clear();
     }
-
-    //transform(texto.begin(), texto.end(), texto.begin(), [](unsigned char c)
-    {
-        return tolower(c);
-    });
-
 
     return 0;
 }
