08f977964bdc07183a506ab6a4bb9002f136453a
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-06 16:10:32 -0300

    feat

--- Exercises-Beecrowd/2502 - Decifrando a Carta Criptografada/main.cpp
+++ Exercises-Beecrowd/2502 - Decifrando a Carta Criptografada/main.cpp
@@ -9,12 +9,17 @@ int main()
 
 
     int n, c ;
-    string cifra1, cifra2;
+    string cifra1, cifra2, frase;
     cin>> n >> c;
 
 
 
-    transform(texto.begin(), texto.end(), texto.begin(), [](unsigned char c)
+    for(int i = 0 ; i < n ; i++)
+    {
+
+    }
+
+    //transform(texto.begin(), texto.end(), texto.begin(), [](unsigned char c)
     {
         return tolower(c);
     });
