0374b696055a5749d2eb12dd3b8b61d6b9f4a6b5
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-05 22:10:25 -0300

    feat

--- Exercises-Beecrowd/2582 - System of a Download/main.cpp
+++ Exercises-Beecrowd/2582 - System of a Download/main.cpp
@@ -5,12 +5,28 @@ using namespace std;
 int main()
 {
 
-    int c , x , y;
+    int c, x, y;
     cin>> c ;
-    for(int i = 0 ; i < c ; i++){
+    for(int i = 0 ; i < c ; i++)
+    {
         cin>> x >> y;
         int opc = (x + y);
-        switch(){
+        switch(opc)
+        {
+
+        case 0:
+
+            break;
+
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
 
 
 
