e122e72d437dcfa0c552d2e70d3106c33b3818ac
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-05 22:14:10 -0300

    feat

--- Exercises-Beecrowd/2582 - System of a Download/main.cpp
+++ Exercises-Beecrowd/2582 - System of a Download/main.cpp
@@ -27,6 +27,30 @@ int main()
         case 3:
 
             break;
+        case 4:
+
+            break;
+
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        case 7:
+
+            break;
+
+        case 8:
+
+            break;
+        case 9:
+
+            break;
+        case 10:
+
+            break;
+
 
 
 
