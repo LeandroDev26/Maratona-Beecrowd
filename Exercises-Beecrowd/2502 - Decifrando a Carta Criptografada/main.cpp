4c9d4ccbbdc356f66ad1566b0b33d8bd2cccc135
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-06 16:03:58 -0300

    feat

--- Exercises-Beecrowd/2502 - Decifrando a Carta Criptografada/main.cpp
+++ Exercises-Beecrowd/2502 - Decifrando a Carta Criptografada/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <cctype>    // Biblioteca para tolower
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -7,13 +9,15 @@ int main()
 
 
     int n, c ;
-    string cifra1 , cifra2;
+    string cifra1, cifra2;
     cin>> n >> c;
 
 
 
-
-
+    transform(texto.begin(), texto.end(), texto.begin(), [](unsigned char c)
+    {
+        return tolower(c);
+    });
 
 
     return 0;
