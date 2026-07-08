392cf2beaf1f396adeb25921ac3e44c42420a95b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-07 21:56:13 -0300

    feat

--- Exercises-Beecrowd/2533 - Estagio/main.cpp
+++ Exercises-Beecrowd/2533 - Estagio/main.cpp
@@ -5,16 +5,19 @@ using namespace std;
 int main()
 {
 
-    int m , c , n ;
+    int m , c , n , ira;
     cout<< fixed << setprecision(4);
     while (cin>> m)
     {
         int totn = 0 , totc = 0;
+        ira = 0;
         for(int i = 0; i < m; i++){
             cin>> n >> c;
              totn += n;
              totc += c;
         }
+
+
 
     }
 
