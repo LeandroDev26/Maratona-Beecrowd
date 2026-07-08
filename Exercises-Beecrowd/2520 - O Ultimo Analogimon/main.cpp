29ca1422d303c1d110b4c4f9d7757357577ccc77
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-08 18:09:49 -0300

    feat

--- Exercises-Beecrowd/2520 - O Ultimo Analogimon/main.cpp
+++ Exercises-Beecrowd/2520 - O Ultimo Analogimon/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
@@ -8,12 +8,13 @@ int main()
     // n e o numero de linhas e o m e o numero de colunas
     while(cin>> n >> m)
     {
+        vector<vector<int>> matriz(n, vector<int>(m));
 
         for(int i = 0 ;  i < n; i++)
         {
             for(int j = 0 ; j < m ; j++)
             {
-
+                cin>>
             }
         }
     }
