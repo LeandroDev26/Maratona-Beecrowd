fe8010f907ec47bb92e60dc6e689bfb5121dc22b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-07 22:02:15 -0300

    feat

--- Exercises-Beecrowd/2533 - Estagio/main.cpp
+++ Exercises-Beecrowd/2533 - Estagio/main.cpp
@@ -4,27 +4,30 @@ using namespace std;
 
 int main()
 {
+    int m, c, n;
+    double ira, totn, totc;
 
-    int m , c , n , ira;
-    cout<< fixed << setprecision(4);
-    while (cin>> m)
+    cout << fixed << setprecision(4);
+
+    while (cin >> m)
     {
-        int totn = 0 , totc = 0;
+        totn = 0;
+        totc = 0;
         ira = 0;
-        for(int i = 0; i < m; i++){
-            cin>> n >> c;
-             totn += n;
-             totc += c;
+
+        for(int i = 0; i < m; i++)
+        {
+            cin >> n >> c;
+
+            totn += (n * c);
+
+            totc += c;
         }
 
-         ira = (totn)/(totc*100);
-         cout<< ira <<endl;
+        ira = (totn) / (totc * 100);
 
+        cout << ira << endl;
     }
-
-
-
-
 
     return 0;
 }
