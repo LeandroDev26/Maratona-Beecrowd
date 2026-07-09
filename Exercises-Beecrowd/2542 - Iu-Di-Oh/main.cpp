567dc5a973cab1940c512920f22842f5394440fd
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-09 08:10:36 -0300

    feat

--- Exercises-Beecrowd/2542 - Iu-Di-Oh/main.cpp
+++ Exercises-Beecrowd/2542 - Iu-Di-Oh/main.cpp
@@ -5,12 +5,17 @@ using namespace std;
 int main()
 {
 
-    int n , m , l , x , y ;
+    int n, m, l, x, y ;
     while(cin>> n)
     {
         cin>> m >> l;
-        for(int i = 0 ; i < (n*m); i++){
-            cin>>
+        for(int i = 0 ; i < (n*m); i++)
+        {
+            cin>> x;
+        }
+        for(int k = 0 ; k < (n*l); k++)
+        {
+            cin>> y;
         }
     }
 
