552c357f3ddd32faaf5644b75bfaa5d1587439d6
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-08 21:01:14 -0300

    feat

--- Exercises-Beecrowd/2540 - Impeachment do Lider/main.cpp
+++ Exercises-Beecrowd/2540 - Impeachment do Lider/main.cpp
@@ -8,7 +8,7 @@ int main()
     int n ;
     while(cin>> n)
     {
-        double notaCorte = 0, favoraveis = 0, x = 0 , totVotos = n;
+        double notaCorte = 0, favoraveis = 0, x = 0, totVotos = n;
         for(int i = 0 ; i < n ; i++)
         {
             cin>> x ;
@@ -18,7 +18,7 @@ int main()
             }
         }
 
-        notaCorte = (n /3)*2;
+        notaCorte = ((totVotos /3.0)*2.0);
         if(favoraveis >= notaCorte)
         {
             cout<< "impeachment"<<endl;
