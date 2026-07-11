79ed7674bee445d8bae1cebaf4a79c4357d2e81f
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-10 21:51:19 -0300

    feat

--- Exercises-Beecrowd/2551 - Novo Recorde/main.cpp
+++ Exercises-Beecrowd/2551 - Novo Recorde/main.cpp
@@ -4,38 +4,27 @@ using namespace std;
 
 int main()
 {
+    int n;
 
-    int n ;
-
-    while(cin>> n)
+    while(cin >> n)
     {
-        double maior_velocidade = 0.0, t = 0.0, d = 0.0;
+
+        double maior_velocidade = -1.0, t = 0.0, d = 0.0;
         double velocidade = 0.0;
-        int dia = 0;
+
         for(int i = 0; i < n; i++)
         {
-            cin>> t >> d;
-            velocidade = (d/t);
+            cin >> t >> d;
+            velocidade = (d / t);
+
+
             if(velocidade > maior_velocidade)
             {
-                dia = (i+1);
-                cout<<dia<<endl;
+                cout << (i + 1) << endl;
                 maior_velocidade = velocidade;
-
             }
-
-
         }
     }
-
-
-
-
-
-
-
-
-
 
     return 0;
 }
