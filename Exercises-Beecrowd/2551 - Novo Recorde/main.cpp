8a7eb9b515fc26ea744044faff4a15e5c9cb04a9
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-10 21:34:58 -0300

    feat

--- Exercises-Beecrowd/2551 - Novo Recorde/main.cpp
+++ Exercises-Beecrowd/2551 - Novo Recorde/main.cpp
@@ -10,9 +10,11 @@ int main()
     while(cin>> n)
     {
         double maior_velocidade = 0.0, t = 0.0, d = 0.0;
+        double velocidade = 0.0;
         for(int i = 0; i < n; i++)
         {
             cin>> t >> d;
+            velocidade = (d/t);
         }
     }
 
