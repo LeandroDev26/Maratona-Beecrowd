67f1ec40686c404daec9972f3a4d3c53e5c82f4b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-13 15:37:49 -0300

    feat

--- Exercises-Beecrowd/2554 - Pizza Antes de BH/main.cpp
+++ Exercises-Beecrowd/2554 - Pizza Antes de BH/main.cpp
@@ -34,7 +34,7 @@ int main()
 
         if(!dataEscolhida.empty())
         {
-            cout<<dataEscolhida;
+            cout<<dataEscolhida<<endl;
             dataEscolhida.clear();
 
         }
