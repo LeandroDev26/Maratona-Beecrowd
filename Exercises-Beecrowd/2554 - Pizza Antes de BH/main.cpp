edab64c573ed3c108ebf05bf2e00dbeabc7c31a8
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-13 15:15:19 -0300

    feat

--- Exercises-Beecrowd/2554 - Pizza Antes de BH/main.cpp
+++ Exercises-Beecrowd/2554 - Pizza Antes de BH/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
 
-    int n, d, ns, ds ;
-    string lixo;
+    int n, d , pessoas;
+    string data;
     while(cin>> n >> d)
     {
        for(int i = 0; i < d ; i++){
+            cin>> data;
 
+            for(int j = 0; j < n ; j++){
+
+            }
        }
 
     }
