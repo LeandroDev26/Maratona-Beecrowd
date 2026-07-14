b00eecab8e996fa7ada72859339122c8aa603426
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-14 19:13:17 -0300

    feat

--- Exercises-Beecrowd/2626 - Turma do JB6/main.cpp
+++ Exercises-Beecrowd/2626 - Turma do JB6/main.cpp
@@ -20,6 +20,19 @@ int main()
         {
             cout<<"Urano perdeu algo muito precioso..."<<endl;
         }
+        // pedra
+        if(dodo == "tesoura" && leo == "papel" && pep == "papel")
+        {
+            cout<<"Os atributos dos monstros vao ser inteligencia, sabedoria..."<<endl;
+        }
+        else if(dodo == "tesoura" && leo == "pedra" && pep == "tesoura")
+        {
+            cout<<"Iron Maiden's gonna get you, no matter how far!"<<endl;
+        }
+        else if(dodo == "tesoura" && leo == "tesoura" && pep == "pedra")
+        {
+            cout<<"Urano perdeu algo muito precioso..."<<endl;
+        }
 
 
     }
