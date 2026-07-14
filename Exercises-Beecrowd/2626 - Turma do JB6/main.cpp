ae530c4a4f461672a5f9487cb7ec0c5ee573faf9
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-14 19:08:54 -0300

    feat

--- Exercises-Beecrowd/2626 - Turma do JB6/main.cpp
+++ Exercises-Beecrowd/2626 - Turma do JB6/main.cpp
@@ -8,6 +8,13 @@ int main()
     string dodo, leo, pep;
     while(cin>> dodo >> leo >> pep)
     {
+        if(dodo == "pedra" && leo == "tesoura" && pep == "tesoura")
+        {
+            cout<<"Os atributos dos monstros vao ser inteligencia, sabedoria..."<<endl;
+        }
+        else if(dodo == "tesoura" && leo == "pedra" && pep == "tesoura")
+        {
+        }
 
     }
 
