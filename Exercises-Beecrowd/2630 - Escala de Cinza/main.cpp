7bf8ba5b42ddae5fc972a6d7dd294bb4f3afa563
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-15 21:14:28 -0300

    feat

--- Exercises-Beecrowd/2630 - Escala de Cinza/main.cpp
+++ Exercises-Beecrowd/2630 - Escala de Cinza/main.cpp
@@ -5,26 +5,34 @@ using namespace std;
 int main()
 {
 
-    int t , r , g , b;
+    int t, r, g, b;
     string conversao;
     cin>> t ;
 
-    for(int i = 0 ; i < t; i++){
-       cin>> conversao >> r >> g >> b;
+    for(int i = 0 ; i < t; i++)
+    {
+        cin>> conversao >> r >> g >> b;
 
-       if(conversao == "min"){
-          int mini = 1000;
+        if(conversao == "min")
+        {
+            int mini = 1000;
 
-           if(r < mini){
-            mini = r;
-           }
-           if(g < mini){
-            mini = g;
-           }
-           if(b < mini){
-            mini = b;
-           }
-       }
+            if(r < mini)
+            {
+                mini = r;
+            }
+            if(g < mini)
+            {
+                mini = g;
+            }
+            if(b < mini)
+            {
+                mini = b;
+            }
+        }
+        else if(conversao ==  "mean")
+        {
+        }
     }
 
     return 0;
