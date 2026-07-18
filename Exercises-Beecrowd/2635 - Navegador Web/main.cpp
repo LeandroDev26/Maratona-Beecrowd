200eb2204ac9ae5fd2eba3b44e23f8c0ca2fd51e
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-17 21:13:25 -0300

    feat

--- Exercises-Beecrowd/2635 - Navegador Web/main.cpp
+++ Exercises-Beecrowd/2635 - Navegador Web/main.cpp
@@ -10,7 +10,7 @@ int main()
     int n, q ;
     string  ns, qs;
     vector <string> pesquisas;
-
+    int maior = 0, cont = 0;
     cin>> n;
     for(int i = 0; i < n; i++)
     {
@@ -23,7 +23,17 @@ int main()
     for(int j = 0; j < q; j++)
     {
         cin>> qs;
-
+        for(int k = 0 ; k < qs.length()-1; k++)
+        {
+            if(qs == pesquisas[k])
+            {
+                cont++;
+                if(maior < pesquisas[k].length())
+                {
+                    maior = pesquisas[k].length();
+                }
+            }
+        }
     }
 
 
