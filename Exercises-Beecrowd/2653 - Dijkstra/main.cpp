c05ba6b94175d5f77d230a8d4090f3b18cfea0c2
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-18 21:38:46 -0300

    feat

--- Exercises-Beecrowd/2653 - Dijkstra/main.cpp
+++ Exercises-Beecrowd/2653 - Dijkstra/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
+
 using namespace std;
 
 int main()
@@ -35,6 +37,24 @@ int main()
 
     }
     cout<< joiasDistintas.size()<<endl;
+
+
+
+        //outra maneira
+
+
+    set<string> joiasDistintas;
+    string joias;
+
+    while (cin >> joias)
+    {
+        // Tentamos inserir a joia.
+        // Se ela já existir lá dentro, o 'set' simplesmente ignora e não faz nada!
+        joiasDistintas.insert(joias);
+    }
+
+    // No final, o tamanho do 'set' será exatamente o número de joias únicas
+    cout << joiasDistintas.size() << endl;
 
 
 
