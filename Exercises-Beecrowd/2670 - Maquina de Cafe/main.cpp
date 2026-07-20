57fa1a759eef50eb4c1e8f51f23bf34158d52fb4
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-20 20:16:40 -0300

    feat

--- Exercises-Beecrowd/2670 - Maquina de Cafe/main.cpp
+++ Exercises-Beecrowd/2670 - Maquina de Cafe/main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-int main()
-{
+int main() {
+    int a1, a2, a3;
 
+    cin >> a1 >> a2 >> a3;
 
-    int a1, a2, a3 , minutos;
-    cin>> a1 >> a2 >> a3;
+    int t1 = (a2 * 2) + (a3 * 4); // Maquina no 1 andar
+    int t2 = (a1 * 2) + (a3 * 2); // Maquina no 2 andar
+    int t3 = (a1 * 4) + (a2 * 2); // Maquina no 3 andar
 
-     minutos = (a1*2 + a3*2);
+    int minutos = min({t1, t2, t3});
 
-     cout<< minutos <<endl;
-
-
-
+    cout << minutos << endl;
 
     return 0;
 }
