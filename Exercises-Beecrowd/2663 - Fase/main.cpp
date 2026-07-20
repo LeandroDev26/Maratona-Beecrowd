2990ab88a788bd49079a230ac8b08f1a817e14d0
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-19 21:25:59 -0300

    feat

--- Exercises-Beecrowd/2663 - Fase/main.cpp
+++ Exercises-Beecrowd/2663 - Fase/main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
 
     int n, k, x;
-
+    vector <int> competidores;
     cin>> n >> k;
     for(int i = 0; i < n ; i++)
     {
         cin>> x;
+        competidores.push_back(x);
     }
 
+    sort(competidores.begin(), competidores.end());
 
 
 
