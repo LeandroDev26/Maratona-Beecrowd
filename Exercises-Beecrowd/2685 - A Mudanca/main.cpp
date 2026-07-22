e93e1bc0aa899ef52b94ab7e9a1f0bc94a461604
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-21 21:58:29 -0300

    feat

--- Exercises-Beecrowd/2685 - A Mudanca/main.cpp
+++ Exercises-Beecrowd/2685 - A Mudanca/main.cpp
@@ -9,9 +9,14 @@ int main()
 
     while(cin>> m)
     {
-         if((m >= 0 && m < 90) || m == 360 ){
+        if((m >= 0 && m < 90) || m == 360 )
+        {
             cout<<"Bom Dia!!"<<endl;
-         }
+        }
+        else if( m >= 90 && m < 180)
+        {
+            cout<<"Boa Tarde!!"<<endl;
+        }
     }
 
 
