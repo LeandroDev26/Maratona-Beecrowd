3b9720e159c24c9f476347f04bedd13659359c6c
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-23 21:53:13 -0300

    feat

--- Exercises-Beecrowd/2686 - A Mudanca Continua/main.cpp
+++ Exercises-Beecrowd/2686 - A Mudanca Continua/main.cpp
@@ -7,7 +7,7 @@ int main()
     double graus ;
     while (cin>> graus)
     {
-        int horas = 0, minutos = 0, segundos = 0, totseg = 0 , segundosrestante = 0 , segundosfinais = 0;
+        int horas = 0, minutos = 0, segundos = 0, totseg = 0, segundosrestante = 0, segundosfinais = 0;
         totseg = (graus*240.0);
         totseg += 21600.0;
         segundos = totseg;
@@ -18,6 +18,23 @@ int main()
         minutos = segundosrestante / 60;
         segundosfinais = segundosrestante % 60;
         segundos = segundosfinais;
+
+        if((graus >= 0 && graus < 90) || graus == 360 )
+        {
+            cout<<"Bom Dia!!"<<endl;
+        }
+        else if( graus >= 90 && graus < 180)
+        {
+            cout<<"Boa Tarde!!"<<endl;
+        }
+        else if( graus >= 180 && graus < 270)
+        {
+            cout<<"Boa Noite!!"<<endl;
+        }
+        else
+        {
+            cout<< "De Madrugada!!"<<endl;
+        }
 
     }
 
