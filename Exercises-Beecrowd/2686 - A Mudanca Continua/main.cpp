ae8f9e848a9752a421af76b91fa572dec025b5b2
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-23 22:09:11 -0300

    feat

--- Exercises-Beecrowd/2686 - A Mudanca Continua/main.cpp
+++ Exercises-Beecrowd/2686 - A Mudanca Continua/main.cpp
@@ -23,27 +23,25 @@ int main()
         if((graus >= 0 && graus < 90) || graus == 360 )
         {
             cout<<"Bom Dia!!"<<endl;
-            cout << setfill('0') << setw(2) << horas << ":" << setw(2) << minutos << ":" << setw(2) << segundos;
         }
         else if( graus >= 90 && graus < 180)
         {
 
             cout<<"Boa Tarde!!"<<endl;
-            cout << setfill('0') << setw(2) << horas << ":" << setw(2) << minutos << ":" << setw(2) << segundos;
 
         }
         else if( graus >= 180 && graus < 270)
         {
             cout<<"Boa Noite!!"<<endl;
-            cout << setfill('0') << setw(2) << horas << ":" << setw(2) << minutos << ":" << setw(2) << segundos;
 
         }
         else
         {
             cout<< "De Madrugada!!"<<endl;
-            cout << setfill('0') << setw(2) << horas << ":" << setw(2) << minutos << ":" << setw(2) << segundos;
 
         }
+        cout << setfill('0') << setw(2) << horas << ":" << setw(2) << minutos << ":" << setw(2) << segundos <<endl;
+
     }
 
 
