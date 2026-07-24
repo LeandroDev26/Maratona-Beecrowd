856bec38cba9c6f968a13391be96843e9ab868c1
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-23 21:47:09 -0300

    feat

--- Exercises-Beecrowd/2686 - A Mudanca Continua/main.cpp
+++ Exercises-Beecrowd/2686 - A Mudanca Continua/main.cpp
@@ -7,14 +7,14 @@ int main()
     double graus ;
     while (cin>> graus)
     {
-        int horas = 0, minutos = 0, segundos = 0, totseg = 0;
+        int horas = 0, minutos = 0, segundos = 0, totseg = 0 , segundosrestante = 0 , segundosfinais = 0;
         totseg = (graus*240.0);
         totseg += 21600.0;
         segundos = totseg;
         segundos = segundos % 86400;
 
         horas = segundos / 3600;
-        minutos = (segundos % 3600)/60;
+
     }
 
 
