49a1c79b3eeab45e1c9e6cc599d009f4393c7537
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-28 11:10:08 -0300

    feat

--- Exercises-Beecrowd/2715 - Dividindo os Trabalhos I/main.cpp
+++ Exercises-Beecrowd/2715 - Dividindo os Trabalhos I/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -12,6 +13,8 @@ int main()
         for(int i = 0; i < n; i++)
         {
             cin>> x;
+            soma += x;
+
         }
 
     }
