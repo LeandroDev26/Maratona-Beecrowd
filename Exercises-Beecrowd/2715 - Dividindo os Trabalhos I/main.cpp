de9fda5c8452e453b4ee4c7e98ddefb80e463b69
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-28 11:08:26 -0300

    feat

---
+++ Exercises-Beecrowd/2715 - Dividindo os Trabalhos I/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    long long n , x;
+    while(cin>> n )
+    {
+        long long  soma = 0, rangel = 0, gugu = 0;
+        for(int i = 0; i < n; i++)
+        {
+            cin>> x;
+        }
+
+    }
+
+
+
+
+
+
+    return 0;
+}
