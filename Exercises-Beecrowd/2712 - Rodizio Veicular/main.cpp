6b0a73d40859e861a3e20469b1f9b40baeb947a5
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-27 22:09:09 -0300

    feat

--- Exercises-Beecrowd/2712 - Rodizio Veicular/main.cpp
+++ Exercises-Beecrowd/2712 - Rodizio Veicular/main.cpp
@@ -11,11 +11,27 @@ int main()
     for(int i = 0; i < n ; i++)
     {
         cin>> placa;
-
+        bool ok = true;
         for(int j = 0 ; j < 3; j++)
         {
             if(placa[i] >= 'A' && placa[j] <= 'Z')
             {
+                if(placa[3] == '-')
+                {
+
+                }
+                else
+                {
+                    ok = false;
+                    cout<<"FAILURE"<<endl;
+                    break;
+                }
+            }
+            else
+            {
+                ok = false;
+                cout<<"FAILURE"<<endl;
+                break;
 
             }
         }
