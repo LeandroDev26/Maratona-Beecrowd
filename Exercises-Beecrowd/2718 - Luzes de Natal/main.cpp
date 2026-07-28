216120093fb3f0a855aa056f6eb4b94f553bce97
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-28 12:14:58 -0300

    feat

--- Exercises-Beecrowd/2718 - Luzes de Natal/main.cpp
+++ Exercises-Beecrowd/2718 - Luzes de Natal/main.cpp
@@ -11,8 +11,16 @@ int main()
     for(int i = 0 ; i < n ; i++)
     {
         cin>> x;
-        int atual = 0 , maximo = 0;
+        int atual = 0, maximo = 0;
+        if(x % 2 == 1)
+        {
+            atual ++;
+            if(atual > maximo)
+            {
+                maximo = atual;
+            }
 
+        }
 
 
     }
