65bd83a11c5006cbe5d8f31668aa73b6250a4484
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-29 21:41:28 -0300

    feat

--- Exercises-Beecrowd/2724 - Ajude Patatatitu/main.cpp
+++ Exercises-Beecrowd/2724 - Ajude Patatatitu/main.cpp
@@ -7,15 +7,23 @@ using namespace std;
 int main()
 {
 
-    int n , t , u;
-    string formulaDanger , elementosJuvenal;
+    int n, t, u;
+    string formulaDanger, elementosJuvenal;
     cin>> n ;
     for(int i = 0 ; i < n; i++)
     {
         cin>> t;
 
-        for(int j = 0; j < t; j++){
+        for(int j = 0; j < t; j++)
+        {
             cin>> formulaDanger;
+        }
+
+        cin>> u;
+
+        for(int k = 0; k < u; k++)
+        {
+            cin>>elementosJuvenal;
         }
 
 
