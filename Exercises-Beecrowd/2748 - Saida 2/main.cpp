f7d91f35278b9c6f140d606f61c763fd90681c3f
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-30 18:37:59 -0300

    feat

--- Exercises-Beecrowd/2748 - Saida 2/main.cpp
+++ Exercises-Beecrowd/2748 - Saida 2/main.cpp
@@ -5,10 +5,33 @@ using namespace std;
 int main()
 {
 
+    int n = 39;
+    for(int i = 0 ; i < n; i++)
+    {
+        cout<<"-";
+    }
+    cout<<endl;
 
-
-
-
+    for(int k = 0; k < 5; k++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(j == n-1 || j == 0)
+            {
+                cout<<"|";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    for(int i = 0 ; i < n; i++)
+    {
+        cout<<"-";
+    }
+    cout<<endl;
 
     return 0;
 }
