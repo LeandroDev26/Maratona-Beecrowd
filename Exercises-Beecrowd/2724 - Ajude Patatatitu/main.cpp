92eaa5bc1314b341f403dc850368b50ceb99964c
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-07-29 22:21:11 -0300

    feat

--- Exercises-Beecrowd/2724 - Ajude Patatatitu/main.cpp
+++ Exercises-Beecrowd/2724 - Ajude Patatatitu/main.cpp
@@ -4,57 +4,107 @@
 #include <cctype>
 using namespace std;
 
-int main()
+
+vector<string> separarBlocos(string formula)
 {
+    vector<string> blocos;
+    string blocoAtual = "";
 
-    int n, t, u;
-    string formulaDanger, elementosJuvenal;
-    vector <string> blocos;
-    cin>> n ;
-    for(int i = 0 ; i < n; i++)
+    for (int y = 0; y < formula.length(); y++)
     {
-
-        vector<vector<string>> listaPerigos;
-
-        cin >> t;
-        for(int j = 0; j < t; j++)
+        if (isupper(formula[y]))
         {
-            cin >> formulaDanger;
-
-
-            vector<string> blocos;
-            string blocoAtual = "";
-
-            for(int y = 0; y < formulaDanger.length(); y++)
-            {
-                if(isupper(formulaDanger[y]))
-                {
-                    if(!blocoAtual.empty())
-                    {
-                        blocos.push_back(blocoAtual);
-                        blocoAtual = "";
-                    }
-                    blocoAtual += formulaDanger[y];
-                }
-                else
-                {
-                    blocoAtual += formulaDanger[y];
-                }
-            }
-
             if (!blocoAtual.empty())
             {
                 blocos.push_back(blocoAtual);
+                blocoAtual = "";
             }
-
-            listaPerigos.push_back(blocos);
+            blocoAtual += formula[y];
+        }
+        else
+        {
+            blocoAtual += formula[y];
         }
     }
+    if (!blocoAtual.empty())
+    {
+        blocos.push_back(blocoAtual);
+    }
+    return blocos;
+}
+
+int main()
+{
+    int n, t, u;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        vector<vector<string>> listaPerigos;
+
+        cin >> t;
+        for (int j = 0; j < t; j++)
+        {
+            string formulaDanger;
+            cin >> formulaDanger;
+            listaPerigos.push_back(separarBlocos(formulaDanger));
+        }
+
+        cin >> u;
+        for (int k = 0; k < u; k++)
+        {
+            string elementosJuvenal;
+            cin >> elementosJuvenal;
+
+            vector<string> experimento = separarBlocos(elementosJuvenal);
+
+            bool explodiu = false;
 
 
+            for (int p = 0; p < listaPerigos.size(); p++)
+            {
+                vector<string> perigo = listaPerigos[p];
+
+                if (perigo.size() > experimento.size()) continue;
+
+                for (int inicio = 0; inicio <= experimento.size() - perigo.size(); inicio++)
+                {
+                    bool match = true;
+                    for (int pos = 0; pos < perigo.size(); pos++)
+                    {
+                        if (experimento[inicio + pos] != perigo[pos])
+                        {
+                            match = false;
+                            break;
+                        }
+                    }
+
+                    if (match)
+                    {
+                        explodiu = true;
+                        break;
+                    }
+                }
+
+                if (explodiu) break;
+            }
+
+            if (explodiu)
+            {
+                cout << "Abortar\n";
+            }
+            else
+            {
+                cout << "Prossiga\n";
+            }
+        }
 
 
-
+        if (i < n - 1)
+        {
+            cout << "\n";
+        }
+    }
 
     return 0;
 }
