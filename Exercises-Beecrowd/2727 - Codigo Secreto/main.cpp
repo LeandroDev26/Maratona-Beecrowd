b1279c2e8818e13f3f2c4768b6244539be79b0a0
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-01 19:53:48 -0300

    feat

--- Exercises-Beecrowd/2727 - Codigo Secreto/main.cpp
+++ Exercises-Beecrowd/2727 - Codigo Secreto/main.cpp
@@ -19,27 +19,27 @@ int main()
         numero++;
     }
 
-    while(cin>> n)
+    cin>> n;
+
+    for(int i = 0; i < n ; i++ )
     {
-        for(int i = 0; i < n ; i++ )
+        getline(cin, cifra);
+
+        int numpont = 0;
+
+        for(int j = 0; j < cifra.length(); j++)
         {
-            getline(cin, cifra);
-
-            int numpont = 0;
-
-            for(int j = 0; j < cifra.length(); j++)
+            if(cifra[j] == '.')
             {
-                if(cifra[j] == '.')
-                {
-                    numpont++;
+                numpont++;
 
-                }
             }
+        }
 
         cout<< alfabeto[numpont]<<endl;
 
-        }
     }
+
 
 
 
