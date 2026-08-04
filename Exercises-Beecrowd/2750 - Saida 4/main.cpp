3829e8e35fd2914b71e5cb3842e7bf48209977d6
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-03 23:18:54 -0300

    feat

--- Exercises-Beecrowd/2750 - Saida 4/main.cpp
+++ Exercises-Beecrowd/2750 - Saida 4/main.cpp
@@ -4,10 +4,21 @@ using namespace std;
 
 int main()
 {
-  cout<<"---------------------------------------"<<endl;
-  cout<<"| decimal   |  octal  |  Hexadecimal  |"<<endl;
-  cout<<"---------------------------------------"<<endl;
-  cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"---------------------------------------"<<endl;
+    cout<<"| decimal   |  octal  |  Hexadecimal  |"<<endl;
+    cout<<"---------------------------------------"<<endl;
+    cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"|      1    |    1    |       1       |"<<endl;
+    cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"|      0    |    0    |       0       |"<<endl;
+
 
 
 
