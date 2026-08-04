adc1a11e2da04e84da96adcf0185a5d9a7900976
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-04 13:39:24 -0300

    feat

--- Exercises-Beecrowd/2754 - Saida 8/main.cpp
+++ Exercises-Beecrowd/2754 - Saida 8/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
 
-
-
+    double primeira = 234.345;
+    double segunda = 45.698;
+    cout<< fixed << setprecision(6);
 
 
 
