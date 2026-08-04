eef8f0044f5566c35fb36b7ec6de817dd484a5f1
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-03 23:38:09 -0300

    feat

--- Exercises-Beecrowd/2750 - Saida 4/main.cpp
+++ Exercises-Beecrowd/2750 - Saida 4/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
@@ -7,15 +6,22 @@ int main() {
     cout << "---------------------------------------" << endl;
     cout << "| decimal   |  octal  |  Hexadecimal  |" << endl;
     cout << "---------------------------------------" << endl;
-
-    for (int i = 0; i < 16; i++) {
-        cout << "|"
-             << setw(7) << dec << i << "    |"
-             << setw(5) << oct << i << "    |"
-             << setw(8) << hex << uppercase << i << "       |"
-             << endl;
-    }
-
+    cout << "|      0    |    0    |       0       |" << endl;
+    cout << "|      1    |    1    |       1       |" << endl;
+    cout << "|      2    |    2    |       2       |" << endl;
+    cout << "|      3    |    3    |       3       |" << endl;
+    cout << "|      4    |    4    |       4       |" << endl;
+    cout << "|      5    |    5    |       5       |" << endl;
+    cout << "|      6    |    6    |       6       |" << endl;
+    cout << "|      7    |    7    |       7       |" << endl;
+    cout << "|      8    |   10    |       8       |" << endl;
+    cout << "|      9    |   11    |       9       |" << endl;
+    cout << "|     10    |   12    |       A       |" << endl;
+    cout << "|     11    |   13    |       B       |" << endl;
+    cout << "|     12    |   14    |       C       |" << endl;
+    cout << "|     13    |   15    |       D       |" << endl;
+    cout << "|     14    |   16    |       E       |" << endl;
+    cout << "|     15    |   17    |       F       |" << endl;
     cout << "---------------------------------------" << endl;
 
     return 0;
