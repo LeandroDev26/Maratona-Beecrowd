b667c153dc276cc02d629a46fd465a24751212bd
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-03 23:22:45 -0300

    feat

--- Exercises-Beecrowd/2750 - Saida 4/main.cpp
+++ Exercises-Beecrowd/2750 - Saida 4/main.cpp
@@ -13,10 +13,10 @@ int main()
     cout<<"|      3    |    3    |       3       |"<<endl;
     cout<<"|      4    |    4    |       4       |"<<endl;
     cout<<"|      5    |    5    |       5       |"<<endl;
-    cout<<"|      0    |    0    |       0       |"<<endl;
-    cout<<"|      0    |    0    |       0       |"<<endl;
-    cout<<"|      0    |    0    |       0       |"<<endl;
-    cout<<"|      0    |    0    |       0       |"<<endl;
+    cout<<"|      6    |    6    |       6       |"<<endl;
+    cout<<"|      7    |    7    |       7       |"<<endl;
+    cout<<"|      8    |   10    |       8       |"<<endl;
+    cout<<"|      9    |   11    |       9       |"<<endl;
     cout<<"|      0    |    0    |       0       |"<<endl;
 
 
