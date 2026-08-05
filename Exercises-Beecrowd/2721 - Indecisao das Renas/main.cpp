7b049e84ff785afaa6ef0a4e83a07a4db029d212
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-05 08:50:44 -0300

    feat

--- Exercises-Beecrowd/2721 - Indecisao das Renas/main.cpp
+++ Exercises-Beecrowd/2721 - Indecisao das Renas/main.cpp
@@ -30,7 +30,39 @@ int main()
 
     case 1:
 
-    break;
+        break;
+
+    case 2:
+
+        break;
+
+    case 3:
+
+        break;
+
+    case 4:
+
+        break;
+
+    case 5:
+
+        break;
+
+    case 6:
+
+        break;
+
+    case 7:
+
+        break;
+
+    case 8:
+
+        break;
+
+    case 9:
+
+        break;
 
 
 
