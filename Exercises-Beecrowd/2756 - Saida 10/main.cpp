6c24eaffc85815052a7683af7c7ade97f927a883
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-06 08:49:24 -0300

    feat

--- Exercises-Beecrowd/2756 - Saida 10/main.cpp
+++ Exercises-Beecrowd/2756 - Saida 10/main.cpp
@@ -2,19 +2,16 @@
 
 using namespace std;
 
-int main()
-{
-
-    cout<<"       A"<<endl;
-    cout<<"      B B"<<endl;
-    cout<<"     C   C"<<endl;
-    cout<<"    D     D"<<endl;
-    cout<<"   E       E"<<endl;
-    cout<<"    D     D"<<endl;
-    cout<<"     C   C"<<endl;
-    cout<<"      B B"<<endl;
-    cout<<"       A"<<endl;
-
+int main() {
+    cout << "       A" << endl;
+    cout << "      B B" << endl;
+    cout << "     C   C" << endl;
+    cout << "    D     D" << endl;
+    cout << "   E       E" << endl;
+    cout << "    D     D" << endl;
+    cout << "     C   C" << endl;
+    cout << "      B B" << endl;
+    cout << "       A" << endl;
 
     return 0;
 }
