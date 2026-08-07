7cd33ecdacb5f9ce5e4d320d30d57dd92f657b83
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-07 09:11:44 -0300

    feat

--- Exercises-Beecrowd/2757 - Entrada e Saida de Numeros Inteiros/main.cpp
+++ Exercises-Beecrowd/2757 - Entrada e Saida de Numeros Inteiros/main.cpp
@@ -5,11 +5,25 @@ using namespace std;
 int main()
 {
     int a, b, c;
-    cin>> a >> b >> c;
-    cout<<"A = "<< a << ", B = "<< b <<", C = "<< c <<endl;
-    cout<<"A = "<< setw(10) << a << "," << " B = "<< setw(10) << b << "," << " C = "<< setw(10) << c <<endl;
-    cout << "A = "<< setfill('0') << right << setw(10) << a << ","<< " B = "<< setfill('0') << right << setw(10) << b << ",123" <<" C = "<< setfill('0') << right << setw(10) << c << endl;
-    cout<<"A = "<< a << setw(10) << "," << " B = "<< b << setw(10) << "," << " C = "<< c << setw(10) <<endl;
+
+    while (cin >> a >> b >> c)
+    {
+
+        cout << "A = " << a << ", B = " << b << ", C = " << c << endl;
+
+        cout << "A = " << right << setw(10) << a
+             << ", B = " << right << setw(10) << b
+             << ", C = " << right << setw(10) << c << endl;
+
+        cout << "A = " << setfill('0') << internal << setw(10) << a
+             << ", B = " << setfill('0') << internal << setw(10) << b
+             << ", C = " << setfill('0') << internal << setw(10) << c << endl;
+
+        cout << setfill(' ');
+        cout << "A = " << left << setw(10) << a
+             << ", B = " << left << setw(10) << b
+             << ", C = " << left << setw(10) << c << endl;
+    }
 
     return 0;
 }
