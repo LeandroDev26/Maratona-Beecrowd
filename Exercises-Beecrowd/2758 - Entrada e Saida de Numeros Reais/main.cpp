3137e4908ba6965b8a250fed11ff2adecfa072f3
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-07 17:41:17 -0300

    docs

--- Exercises-Beecrowd/2758 - Entrada e Saida de Numeros Reais/main.cpp
+++ Exercises-Beecrowd/2758 - Entrada e Saida de Numeros Reais/main.cpp
@@ -5,26 +5,6 @@ using namespace std;
 int main()
 {
 
-    int notasvalidas = 0;
-    int nota, soma = 0;
-
-    do
-    {
-        cin>>nota;
-        if(nota == -1) break;
-        if(nota < 0 || nota > 10) continue;
-        notasvalidas++;
-        soma += nota;
-    }
-    while(notasvalidas < 5);
-
-    int media = soma/5;
-
-    cout<<media<<endl;
-
-
-
-
 
 
 
