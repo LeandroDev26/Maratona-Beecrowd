d63b2385262aa563760ca06a9a34f9cee695bf1b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-07 08:53:33 -0300

    feat

--- Exercises-Beecrowd/2757 - Entrada e Saida de Numeros Inteiros/main.cpp
+++ Exercises-Beecrowd/2757 - Entrada e Saida de Numeros Inteiros/main.cpp
@@ -7,16 +7,9 @@ int main()
     int a, b, c;
     cin>> a >> b >> c;
     cout<<"A = "<< a << ", B = "<< b <<", C = "<< c <<endl;
-
-
-
-
-
     cout<<"A = "<< setw(10) << a << "," << " B = "<< setw(10) << b << "," << " C = "<< setw(10) << c <<endl;
-
-    //1 cout<< left << setw(10) << a << endl;
-
-    cout<< setfill('0') << right << setw(10) << c << endl;
+    cout << "A = "<< setfill('0') << right << setw(10) << a << ","<< " B = "<< setfill('0') << right << setw(10) << b << ",123" <<" C = "<< setfill('0') << right << setw(10) << c << endl;
+    cout<<"A = "<< setw(10) << a << "," << " B = "<< setw(10) << b << "," << " C = "<< setw(10) << c <<endl;
 
     return 0;
 }
