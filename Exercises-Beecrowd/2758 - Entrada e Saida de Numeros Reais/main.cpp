52bab1b2200cfc724b9c256e589a51ac459ac318
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-08 18:29:44 -0300

    feat

--- Exercises-Beecrowd/2758 - Entrada e Saida de Numeros Reais/main.cpp
+++ Exercises-Beecrowd/2758 - Entrada e Saida de Numeros Reais/main.cpp
@@ -6,9 +6,11 @@ int main()
 {
     double a, b, c, d ;
     string lixo;
-    while(cin>> a >> lixo >> b >> c >> lixo >> d){
+    while(cin>> a >> lixo >> b >> c >> lixo >> d)
+    {
 
-
+        cout<< fixed << setprecision(6) << "A = "<< a << ","<< " B = "<< b <<endl;
+        cout<<"C = "<< c << ","<< " D = "<< d <<endl;
     }
 
 
