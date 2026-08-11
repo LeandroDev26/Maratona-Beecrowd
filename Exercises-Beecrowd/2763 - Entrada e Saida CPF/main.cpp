f3f69c63f25e2b23612d7f8ec6a7005734d4bd58
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-10 23:22:01 -0300

    feat

--- Exercises-Beecrowd/2763 - Entrada e Saida CPF/main.cpp
+++ Exercises-Beecrowd/2763 - Entrada e Saida CPF/main.cpp
@@ -8,11 +8,26 @@ int main()
     string pont1, pont2, hifen;
     while (cin>> num1 >> pont1 >> num2 >> pont2 >> num3 >> hifen >> num4 )
     {
-        cout<< setfill('0') << setw(3) << num1 <<endl;
+        if(num1 != 0 ){
+        cout<< setw(3) << num1 <<endl;
+
+        }
+
+
+
+
+
+
+        cout<< setw(3) << num1 <<endl;
+        cout<<  setw(3) << num2 <<endl;
+        cout<< setw(3) << num3 <<endl;
+        cout<< setw(2) << num4 <<endl;
+
+
+           cout<< setfill('0') << setw(3) << num1 <<endl;
         cout<< setfill('0') << setw(3) << num2 <<endl;
         cout<< setfill('0')<<setw(3) << num3 <<endl;
         cout<< setfill ('0')<<setw(2) << num4 <<endl;
-
     }
 
 
