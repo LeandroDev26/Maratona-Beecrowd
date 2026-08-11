4871035636f67ea2da41e723a60e2a9ac4240efe
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-10 23:24:07 -0300

    feat

--- Exercises-Beecrowd/2763 - Entrada e Saida CPF/main.cpp
+++ Exercises-Beecrowd/2763 - Entrada e Saida CPF/main.cpp
@@ -8,22 +8,32 @@ int main()
     string pont1, pont2, hifen;
     while (cin>> num1 >> pont1 >> num2 >> pont2 >> num3 >> hifen >> num4 )
     {
-        if(num1 != 0 ){
-        cout<< setw(3) << num1 <<endl;
+        if(num1 != 0 )
+        {
+            cout<< setw(3) << num1 <<endl;
 
         }
-        if(num2 != 0 ){
-        cout<< setw(3) << num2<<endl;
+        else
+        {
+            cout<< setfill('0') << setw(3) << num1 <<endl;
 
         }
-        if(num3 != 0 ){
-        cout<< setw(3) << num3 <<endl;
+        if(num2 != 0 )
+        {
+            cout<< setw(3) << num2<<endl;
 
         }
-        if(num4 != 0 ){
-        cout<< setw(3) << num4 <<endl;
+        if(num3 != 0 )
+        {
+            cout<< setw(3) << num3 <<endl;
 
         }
+        if(num4 != 0 )
+        {
+            cout<< setw(3) << num4 <<endl;
+
+        }
+        else {}
 
 
 
@@ -33,13 +43,8 @@ int main()
 
 
 
-        cout<< setw(3) << num1 <<endl;
-        cout<<  setw(3) << num2 <<endl;
-        cout<< setw(3) << num3 <<endl;
-        cout<< setw(2) << num4 <<endl;
 
 
-           cout<< setfill('0') << setw(3) << num1 <<endl;
         cout<< setfill('0') << setw(3) << num2 <<endl;
         cout<< setfill('0')<<setw(3) << num3 <<endl;
         cout<< setfill ('0')<<setw(2) << num4 <<endl;
