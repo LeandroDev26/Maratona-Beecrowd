5ad5b7d5ec02f1882f736144519eb45dc3ef9c5a
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-11 13:17:24 -0300

    feat

--- Exercises-Beecrowd/2760 - Entrada e Saida de String/main.cpp
+++ Exercises-Beecrowd/2760 - Entrada e Saida de String/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
     string a, b, c ;
 
+    while(getline(cin, a), getline(cin, b), getline(cin, c) )
+    {
+        if(a.length() > 100 || b.length() > 100 || c.length() > 100) continue;
 
+
+
+
+    }
 
 
 
