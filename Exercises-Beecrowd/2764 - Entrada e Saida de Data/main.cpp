3a1fe78441b3ad821be2cf76595acaecbe787bf7
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-12 15:50:03 -0300

    feat

--- Exercises-Beecrowd/2764 - Entrada e Saida de Data/main.cpp
+++ Exercises-Beecrowd/2764 - Entrada e Saida de Data/main.cpp
@@ -10,7 +10,7 @@ int main()
 
     while(cin>> dia >> barra1 >> mes >> barra2 >> ano)
     {
-
+        cout<< setfill('0') << setw(2) << dia << barra1 << setw(2) << mes << barra2 << setw(2) << ano<<endl;
     }
 
 
