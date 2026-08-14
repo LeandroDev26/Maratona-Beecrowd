cf93bd345119a9d2d43bce0d82597e885293ebe6
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-14 17:33:15 -0300

    feat

--- Exercises-Beecrowd/2769 - Linha de Montagem/main.cpp
+++ Exercises-Beecrowd/2769 - Linha de Montagem/main.cpp
@@ -6,10 +6,16 @@ int main()
 {
 
     int n, e1, e2;
+    vector < int > linha1;
+    vector < int > linha2;
 
     while(cin>> n)
     {
-
+        cin>> e1 >> e2;
+        for(int i = 0 ; i < n ; i++)
+        {
+            cin>>
+        }
     }
 
 
