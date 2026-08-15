ec80976c8d5a536740d0d3d62d2590d6659a842b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-14 23:16:09 -0300

    feat

--- Exercises-Beecrowd/2769 - Linha de Montagem/main.cpp
+++ Exercises-Beecrowd/2769 - Linha de Montagem/main.cpp
@@ -6,22 +6,45 @@ using namespace std;
 int main()
 {
 
-    int n, e1, e2 , tempoLinha1 , tempoLinha2;
+    int n, e1, e2;
     vector < int > linha1;
     vector < int > linha2;
-
+    vector < int > linha1;
+    vector < int > linha2;
     while(cin>> n)
     {
         cin>> e1 >> e2;
         for(int i = 0 ; i < n ; i++)
         {
-
+            int z;
+            cin>> z;
+            linha1.push_back(z);
         }
 
         for(int j = 0 ; j < n ; j++)
         {
+            int z;
+            cin>> z;
+            linha2.push_back(z);
 
         }
+
+        for(int l = 1 ; l < n ; l++)
+        {
+            int z;
+            cin>> z;
+            linha1.push_back(z);
+        }
+
+        for(int k = 1 ; k < n ; k++)
+        {
+            int z;
+            cin>> z;
+            linha2.push_back(z);
+
+        }
+
+
     }
 
 
