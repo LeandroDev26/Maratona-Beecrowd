459c06922aeeb2cbdb43f40bac4149b43db0c551
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-15 16:06:45 -0300

    feat

--- Exercises-Beecrowd/2770 - Tamanho da Placa/main.cpp
+++ Exercises-Beecrowd/2770 - Tamanho da Placa/main.cpp
@@ -2,35 +2,24 @@
 
 using namespace std;
 
-int main()
-{
-
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int x, y, m, xi, yi;
-    while(cin>> x >> y >> m)
-    {
 
-        for(int i = 0; i < m ; i++)
-        {
+    while(cin >> x >> y >> m) {
+        for(int i = 0; i < m ; i++) {
+            cin >> xi >> yi;
 
-            cin>> xi >> xi;
-            if( xi <= x && yi <= y || xi <= y && yi <= x)
-            {
-
-
+            if((xi <= x && yi <= y) || (xi <= y && yi <= x)) {
+                cout << "Sim\n";
             }
-
+            else {
+                cout << "Nao\n";
+            }
         }
-
-
     }
-
-
-
-
-
-
-
 
     return 0;
 }
