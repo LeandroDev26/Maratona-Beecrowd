5dc81895f5c53bfdcff255d50ce51a6dbf8c4eb5
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-16 21:30:50 -0300

    feat

--- Exercises-Beecrowd/2779 - Album da Copa/main.cpp
+++ Exercises-Beecrowd/2779 - Album da Copa/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -7,8 +8,14 @@ int main()
 
     int n, m;
     cin>> n >> m ;
+    vector<int> numeros;
+
+    for(int i = 0 ; i < m; i++){
 
 
+           auto it = std::find(numeros.begin(), numeros.end(), valor);
+
+    }
 
 
 
