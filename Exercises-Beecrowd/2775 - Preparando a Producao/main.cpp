9d29cb5baa26e371b5b5ec28c868c1be63cbe4ee
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-19 09:06:52 -0300

    feat

--- Exercises-Beecrowd/2775 - Preparando a Producao/main.cpp
+++ Exercises-Beecrowd/2775 - Preparando a Producao/main.cpp
@@ -5,10 +5,21 @@ using namespace std;
 int main()
 {
 
-   int n ;
-   while(cin>> n){
+    int n ;
+    while(cin>> n)
+    {
 
-   }
+
+        for(int i = 0 ; i < n; i++)
+        {
+
+        }
+
+        for(int j = 0 ; j < n ; j++)
+        {
+
+        }
+    }
 
 
 
