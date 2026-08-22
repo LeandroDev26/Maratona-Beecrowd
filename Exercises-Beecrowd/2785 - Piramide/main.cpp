abfa2f7cdd0a4db34db06e43b8f78f80a1298377
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-22 09:18:16 -0300

    feat

--- Exercises-Beecrowd/2785 - Piramide/main.cpp
+++ Exercises-Beecrowd/2785 - Piramide/main.cpp
@@ -17,15 +17,26 @@ int main()
 
         for(int j = 0 ; j < n ; j++)
         {
-              cin>> matriz[i][j];
+            cin>> matriz[i][j];
+        }
+    }
+
+    vector<vector<int>> prefixos(n, vector<int>(n, 0));
+
+
+    for(int i = 0; i < n; i++)
+    {
+        prefixos[i][0] = matriz[i][0];
+
+        for(int j = 1; j < n; j++)
+        {
+            prefixos[i][j] = prefixos[i][j-1] + matriz[i][j];
         }
     }
 
 
 
-
-
-     for(int i = 0 ; i < n; i++)
+    for(int i = 0 ; i < n; i++)
     {
 
 
