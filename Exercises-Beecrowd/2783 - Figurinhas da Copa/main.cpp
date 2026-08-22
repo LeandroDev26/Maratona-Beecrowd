087d86ca2e787aa4c257f65ddfae817eb66ba758
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-21 23:13:37 -0300

    feat

--- Exercises-Beecrowd/2783 - Figurinhas da Copa/main.cpp
+++ Exercises-Beecrowd/2783 - Figurinhas da Copa/main.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    int n, c, m , xi , yi;
+    int n, c, m, xi, yi;
+    vector<int> figurinhasCarimbadas;
+    vector<int>figurinhasCompradas;
 
     cin>> n >> c >> m;
 
-    for(int i = 0 ; i < c ; i++){
+    for(int i = 0 ; i < c ; i++)
+    {
         cin>> xi;
     }
 
-    for(int j = 0; j < m; j++){
+    for(int j = 0; j < m; j++)
+    {
         cin>>yi;
     }
 
+
+    for(int l = 0 ; l < c ; l++)
+    {
+
+
+        for(int k = 0 ; k < m; k++)
+        {
+
+        }
+    }
 
 
 
