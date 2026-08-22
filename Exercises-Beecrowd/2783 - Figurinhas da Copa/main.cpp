8d7480494f374ee742f2cb25a567241a623fc184
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-21 23:03:50 -0300

    feat

--- Exercises-Beecrowd/2783 - Figurinhas da Copa/main.cpp
+++ Exercises-Beecrowd/2783 - Figurinhas da Copa/main.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main()
 {
-    int n, c, m , xi ;
+    int n, c, m , xi , yi;
 
     cin>> n >> c >> m;
 
     for(int i = 0 ; i < c ; i++){
         cin>> xi;
+    }
+
+    for(int j = 0; j < m; j++){
+        cin>>yi;
     }
 
 
