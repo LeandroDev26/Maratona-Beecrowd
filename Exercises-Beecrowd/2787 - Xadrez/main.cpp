4a095e9d22fe08e9247fe9a866a61760a5845d9a
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-24 23:28:32 -0300

    feat

--- Exercises-Beecrowd/2787 - Xadrez/main.cpp
+++ Exercises-Beecrowd/2787 - Xadrez/main.cpp
@@ -11,7 +11,12 @@ int main()
 
     cin>> l >> c;
 
-    if(l % 2 == 1 && c % 2 == 1){
+    if(l % 2 == 1 && c % 2 == 1)
+    {
+        cout<< 1 <<endl;
+    }
+    else if(l % 2 == 0 && c % 2 == 0)
+    {
         cout<< 1 <<endl;
     }
 
