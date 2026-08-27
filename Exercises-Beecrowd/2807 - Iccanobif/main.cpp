4220adab9e7eab3a254bc7714a1ca78915870432
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-27 09:28:56 -0300

    feat

--- Exercises-Beecrowd/2807 - Iccanobif/main.cpp
+++ Exercises-Beecrowd/2807 - Iccanobif/main.cpp
@@ -4,40 +4,43 @@ using namespace std;
 
 int main()
 {
+    int n, atual = 0;
+    cin >> n;
 
-    vector<int> fibonacci;
-    int n, atual = 0 ;
-    cin>> n;
-    if(n == 1)
+    // Trata casos inválidos ou zero
+    if (n <= 0)
+    {
+        return 0;
+    }
+
+    vector<int> fibonacci(n);
+
+    if (n == 1)
     {
         fibonacci[0] = 1;
-
     }
     else
     {
         fibonacci[0] = 1;
         fibonacci[1] = 1;
-        if(n == 2)
-        {
-            cout<<fibonacci[1] <<" " << fibonacci[0]<<" " <<endl;
-        }
-
     }
+
     for(int i = 2 ; i < n; i++)
     {
-
         atual = (fibonacci[i-1] + fibonacci[i-2]);
-        fibonacci.push_back(atual);
+        fibonacci[i] = atual;
     }
-
 
     for(int j = n-1; j >= 0; j--)
     {
-        cout<<fibonacci[j]<<" ";
+        cout << fibonacci[j];
+        if (j > 0)
+        {
+            cout << " ";
+        }
     }
 
-    cout<<endl;
-
+    cout << endl;
 
     return 0;
 }
