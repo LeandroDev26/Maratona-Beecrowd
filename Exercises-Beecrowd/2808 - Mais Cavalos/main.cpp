d551f9ddb0e3556ad2e8afa4cfb71bacf44adf2b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-28 09:53:51 -0300

    feat

--- Exercises-Beecrowd/2808 - Mais Cavalos/main.cpp
+++ Exercises-Beecrowd/2808 - Mais Cavalos/main.cpp
@@ -9,10 +9,10 @@ int main()
     char col1, col2;
     int lin1, lin2;
 
-   cin>> col1 >> lin1 >> col2 >> lin2;
+    cin>> col1 >> lin1 >> col2 >> lin2;
 
-
-
+    int difeColuna = abs(col1 - col2);
+    int difeLinha = abs(lin1 - lin2);
 
 
     return 0;
