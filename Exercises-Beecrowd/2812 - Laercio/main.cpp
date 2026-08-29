60943e5d96233517fa81842c3271b36d5e964ce8
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-29 15:39:47 -0300

    feat

--- Exercises-Beecrowd/2812 - Laercio/main.cpp
+++ Exercises-Beecrowd/2812 - Laercio/main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
 
-    int n , m;
+    int n, m;
 
     while(cin>> n)
     {
+        vector <int> lista;
+        cin>> m;
 
+        for(int i = 0 ; i < m ; i++){
+
+        }
     }
 
 
