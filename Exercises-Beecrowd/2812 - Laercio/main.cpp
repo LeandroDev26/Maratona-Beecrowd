e5ca529bbb6508ea1656769d15dbeeaa8cf1b7be
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-29 15:53:42 -0300

    feat

--- Exercises-Beecrowd/2812 - Laercio/main.cpp
+++ Exercises-Beecrowd/2812 - Laercio/main.cpp
@@ -11,18 +11,21 @@ int main()
     while(cin>> n)
     {
         vector <int> lista;
+
         cin>> m;
 
         for(int i = 0 ; i < m ; i++)
         {
             cin>> mi;
-            lista.push_back(mi);
+            if(mi%2 == 1)
+            {
+                lista.push_back(mi);
+
+            }
         }
 
 
-        for(int j = 0 ; j < m ; j++){
 
-        }
     }
 
 
