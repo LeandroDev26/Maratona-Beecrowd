236ebc80bc0ef3c1a858e0eba16c10cc470e5525
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-30 20:19:54 -0300

    feat

--- Exercises-Beecrowd/2813 - Evitando Chuva/main.cpp
+++ Exercises-Beecrowd/2813 - Evitando Chuva/main.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main()
 {
 
-    int n , comprados_casa = 0 , comprados_escritorio = 0 , sobrando_casa = 0 , sobrando_escritorio = 0;
-    string ida , volta;
+    int n, comprados_casa = 0, comprados_escritorio = 0, sobrando_casa = 0, sobrando_escritorio = 0;
+    string ida, volta;
     cin>> n;
 
     for(int i = 0 ; i < n ; i++)
     {
         cin>> ida >> volta;
+        if(ida == "chuva")
+        {
+
+        }
 
     }
 
