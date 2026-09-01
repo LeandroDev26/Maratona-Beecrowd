e567e7813132dba1e26a8f3627cafccf2ea83b32
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-08-31 23:21:30 -0300

    feat

--- Exercises-Beecrowd/2823 - Eearliest Deadline First/main.cpp
+++ Exercises-Beecrowd/2823 - Eearliest Deadline First/main.cpp
@@ -7,13 +7,15 @@ int main()
 
 
     int n ;
-    double c, p, soma = 0;
+    double c, p, soma = 0.0;
     cin>> n ;
 
     for(int i = 0 ; i < n; i++)
     {
 
         cin>> c >> p;
+        double u = c/p;
+        soma += u;
     }
 
 
