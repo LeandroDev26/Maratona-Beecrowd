136ce06e0fca69b1afe86bdf49b17f4e5f48aa2e
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-01 23:05:51 -0300

    feat

--- Exercises-Beecrowd/2826 - Lexico/main.cpp
+++ Exercises-Beecrowd/2826 - Lexico/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
+int main() {
+    string palavra1, palavra2;
 
+    cin >> palavra1 >> palavra2;
 
-
-
-
-
-
+    if (palavra1 <= palavra2) {
+        cout << palavra1 << "\n";
+        cout << palavra2 << "\n";
+    } else {
+        cout << palavra2 << "\n";
+        cout << palavra1 << "\n";
+    }
 
     return 0;
 }
