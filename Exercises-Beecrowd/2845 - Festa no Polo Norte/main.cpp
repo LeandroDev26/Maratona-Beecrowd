83abf020d572aacb34bfe5c6acaec127e4fa54de
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-02 09:16:50 -0300

    feat

--- Exercises-Beecrowd/2845 - Festa no Polo Norte/main.cpp
+++ Exercises-Beecrowd/2845 - Festa no Polo Norte/main.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
-int main()
-{
-
-
+int main() {
     int n, ai;
-    vector<int> lista;
-    cin>> n;
+    cin >> n;
 
-    for(int i = 0 ; i < n; i++)
-    {
-        cin>> ai;
-        lista.push_back(ai);
-    }
-    int maior = lista[0];
-
-    for(int j = 1; j < n; j++)
-    {
-        if(maior < lista[j])
-        {
-            maior = lista[j];
+    int maior = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> ai;
+        if (ai > maior) {
+            maior = ai;
         }
     }
 
-    cout<< maior+1 <<endl;
-
-
-
-
-
-
-
-
+    cout << maior + 1 << endl;
 
     return 0;
 }
