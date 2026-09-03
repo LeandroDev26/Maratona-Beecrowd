8fb3eedcd316b1d5d431daf59e6c309145d523e3
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-03 09:15:59 -0300

    feat

--- Exercises-Beecrowd/2846 - Fibonot/main.cpp
+++ Exercises-Beecrowd/2846 - Fibonot/main.cpp
@@ -6,11 +6,14 @@ int main()
 {
 
     int k ;
-    int a = 2 , b = 3 , totfibonot = 0 , anterior = 0;
+    int a = 2, b = 3, totfibonot = 0, anterior = 0;
     cin>> k ;
 
-    while(totfibonot < k){
-
+    while(totfibonot < k)
+    {
+        anterior = totfibonot;
+        int quantidade = (b - a)- 1;
+        totfibonot += quantidade;
     }
 
 
