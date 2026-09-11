838ce2bbbfc6600f1296a9c4d9df7fb2acffee66
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-10 21:08:13 -0300

    feat

--- Exercises-Beecrowd/2936 - Quanta Mandioca/main.cpp
+++ Exercises-Beecrowd/2936 - Quanta Mandioca/main.cpp
@@ -4,19 +4,9 @@ using namespace std;
 
 int main()
 {
-
     int  porc1 , porc2 , porc3 , porc4 , porc5 ;
     cin>> porc1 >> porc2 >> porc3 >> porc4 >> porc5 ;
     int total = ((porc1*300)+(porc2*1500)+(porc3*600)+(porc4*1000)+(porc5*150) + 225);
     cout<<total<<endl;
-
-
-
-
-
-
-
-
-
     return 0;
 }
