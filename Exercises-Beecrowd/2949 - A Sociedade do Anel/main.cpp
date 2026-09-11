1e7099e67ba5e5fc43ab0df8d6a71179a77269ad
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-11 09:51:57 -0300

    feat

--- Exercises-Beecrowd/2949 - A Sociedade do Anel/main.cpp
+++ Exercises-Beecrowd/2949 - A Sociedade do Anel/main.cpp
@@ -6,11 +6,19 @@ int main()
 {
     string nome ;
     char tipo;
-    int n;
+    int n, hobbits = 0, humanos = 0, elfos = 0, magos = 0, anoes = 0 ;
 
     for(int i = 0; i < n; i++)
     {
-       cin>> nome >> tipo;
+        cin>> nome >> tipo;
+        if(tipo == 'A')
+        {
+            anoes++;
+        }
+        else  if(tipo == 'E')
+        {
+            elfos++;
+        }
     }
 
 
