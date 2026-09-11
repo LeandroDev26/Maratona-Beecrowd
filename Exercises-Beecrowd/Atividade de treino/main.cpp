c5dd18b5d9c748b12de6b9e252f6ca4e833cc6b2
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-11 16:22:38 -0300

    feat

--- Exercises-Beecrowd/Atividade de treino/main.cpp
+++ Exercises-Beecrowd/Atividade de treino/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include <array>
+#include <utility>
+#include <vector>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main() {
+    array<int,9> estado = {1,2,3,4,0,6,7,5,8}; // 0 é o vazio
+
+    // encontrar a posição do 0
+    int pos = -1;
+    for (int i = 0; i < 9; i++) {
+        if (estado[i] == 0) { pos = i; break; }
+    }
+
+    int linha = pos / 3;
+    int coluna = pos % 3;
+    cout << "Zero esta na posicao " << pos
+         << " (linha " << linha << ", coluna " << coluna << ")\n";
+
+  vector<array<int,9>> sucessores;
+
 }
