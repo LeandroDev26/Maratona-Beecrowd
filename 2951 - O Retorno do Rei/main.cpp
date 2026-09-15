277b5ffb5229cad57d4bd26763a5be0ff4f16655
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-15 14:34:48 -0300

    feat

--- 2951 - O Retorno do Rei/main.cpp
+++ 2951 - O Retorno do Rei/main.cpp
@@ -4,15 +4,23 @@ using namespace std;
 
 int main()
 {
-  // g = tot necessario para vencer e n = casos de teste x = a qtd de vezes que recitaram
-      map<char , int> runas;
-    int n, g , x , ri ;
-    char vi , runaRs;
+    // g = tot necessario para vencer e n = casos de teste x = a qtd de vezes que recitaram
+    map<char, int> runas;
+    int n, g, x, ri ;
+    char vi, runaRs;
     cin>> n >> g;
-    for(int i = 0 ; i < n; i++){
+    for(int i = 0 ; i < n; i++)
+    {
         cin>> vi >> ri;
         runas.insert({vi, ri});
     }
+    cin>> x ;
+
+    for(int j = 0 ; j < x; j++)
+    {
+
+    }
+
 
 
 
