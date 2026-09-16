2d7971e3964fd575e28857fc5ef11e8a9ecfd7c2
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-16 08:28:17 -0300

    feat

--- 2963 - Bobo da Corte/main.cpp
+++ 2963 - Bobo da Corte/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
 
-    int n ;
+    int n , xi ;
+    vector<int> listaDeCandidatos;
     cin>> n;
     for(int i = 0 ; i < n ; i++)
     {
+        cin>> xi;
 
     }
 
