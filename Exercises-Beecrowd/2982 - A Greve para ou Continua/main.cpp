2f337707fe7a9345620bb9623ba560a66b2593fa
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-16 17:26:51 -0300

    feat

--- Exercises-Beecrowd/2982 - A Greve para ou Continua/main.cpp
+++ Exercises-Beecrowd/2982 - A Greve para ou Continua/main.cpp
@@ -4,13 +4,23 @@ using namespace std;
 
 int main()
 {
-    int n, c ;
+    int n, c, gastos = 0, verba = 0;
     char t;
 
     cin>> n;
     for(int i = 0 ; i < n ; i++)
     {
         cin>> t >> c;
+
+        if(t == 'V')
+        {
+
+        }
+        else
+        {
+
+        }
+
     }
 
 
