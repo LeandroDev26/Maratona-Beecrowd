d92048ac81335cae2d48b397ab9c9e7b8b3a1b7c
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-16 19:24:15 -0300

    feat

--- Exercises-Beecrowd/3037 - Jogando Dardos Por Distancia/main.cpp
+++ Exercises-Beecrowd/3037 - Jogando Dardos Por Distancia/main.cpp
@@ -5,13 +5,18 @@ using namespace std;
 int main()
 {
 
-   int n , x , d ;
-   cin>> n ;
+    int n, x, d ;
+    cin>> n ;
 
-   for(int i = 0 ; i < n ; i++){
+    for(int i = 0 ; i < n ; i++)
+    {
 
-      cin>> x >> d;
-   }
+        for(int j = 0 ; j < 6 ; j++)
+        {
+            cin>> x >> d;
+
+        }
+    }
 
 
 
