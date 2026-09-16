86ea9e48183973631d06654f4ef67ac9d36b116b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-16 19:02:45 -0300

    feat

---
+++ Exercises-Beecrowd/2987 - Balao de Honra/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char l ;
+    cin>> l;
+    /*for(char i = 'a' ; i <= 'z'; i++){
+      cout<< i << " " <<endl;
+    }
+    */
+
+
+
+
+
+    return 0;
+}
