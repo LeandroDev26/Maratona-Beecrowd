b2c58903a808a148f8c71ebe9617afed468feb27
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-16 08:34:27 -0300

    feat

--- 2963 - Bobo da Corte/main.cpp
+++ 2963 - Bobo da Corte/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 
-    int n , xi ;
+    int n, xi ;
     vector<int> listaDeCandidatos;
     cin>> n;
     for(int i = 0 ; i < n ; i++)
@@ -16,8 +16,19 @@ int main()
     }
     bool foiEleito = true;
     int carlos = listaDeCandidatos[0];
-    for(int j = 1; j < n; j++){
+    for(int j = 1; j < n; j++)
+    {
+        if(carlos >= listaDeCandidatos[j])continue;
+        foiEleito = false;
+    }
 
+    if(foiEleito)
+    {
+        cout<<"S"<<endl;
+    }
+    else
+    {
+        cout<<"N"<<endl;
     }
 
 
