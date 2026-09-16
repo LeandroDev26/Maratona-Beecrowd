505a2578907e8f85494644590dea5edd38e093a8
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-16 19:04:50 -0300

    feat

--- Exercises-Beecrowd/2987 - Balao de Honra/main.cpp
+++ Exercises-Beecrowd/2987 - Balao de Honra/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
     char l ;
+    vector<char> alfabeto;
     cin>> l;
-    /*for(char i = 'a' ; i <= 'z'; i++){
-      cout<< i << " " <<endl;
+    for(char i = 'a' ; i <= 'z'; i++)
+    {
+        cout<< i << " " <<endl;
+        alfabeto.push_back(i);
+
     }
-    */
+
 
 
 
