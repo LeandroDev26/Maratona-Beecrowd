87338eb176f70d66339c8dcccaddab5fc8b2421f
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-16 09:25:19 -0300

    feat

--- 2968 - Hora da Corrida/main.cpp
+++ 2968 - Hora da Corrida/main.cpp
@@ -13,6 +13,15 @@ int main()
     for(int i = 10 ; i <= 90; i+=10)
     {
         int atualPorc = totplacas * (i /100);
+        if( i < 90)
+        {
+            cout<< atualPorc + 1 << " " ;
+
+        }
+        else
+        {
+            cout<< atualPorc + 1 <<endl;
+        }
     }
 
 
