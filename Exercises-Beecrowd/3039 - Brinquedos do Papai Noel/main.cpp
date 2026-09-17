bf47e450f73f2a72bc61d543c66aca8800b49e4c
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-17 06:20:16 -0300

    feat

--- Exercises-Beecrowd/3039 - Brinquedos do Papai Noel/main.cpp
+++ Exercises-Beecrowd/3039 - Brinquedos do Papai Noel/main.cpp
@@ -5,6 +5,15 @@ using namespace std;
 int main()
 {
 
+    int n ;
+    string nome ;
+    char sexo;
+    cin>> n;
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin>> nome >> sexo;
+    }
 
 
 
