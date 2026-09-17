1ae2f9b9aec9764c7987c9eebd6808136a8c8932
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-17 06:35:02 -0300

    feat

--- Exercises-Beecrowd/3040 - A Arvore de Natal/main.cpp
+++ Exercises-Beecrowd/3040 - A Arvore de Natal/main.cpp
@@ -5,14 +5,23 @@ using namespace std;
 int main()
 {
 
-    int n , h , d , g ;
+    int n, h, d, g ;
     cin>> n;
 
-   for(int i = 0 ; i < n ; i++){
+    for(int i = 0 ; i < n ; i++)
+    {
 
-         cin>> h >> d >> g;
+        cin>> h >> d >> g;
 
-   }
+        if((h >= 200 && h <= 300) && (d >= 50) && (g >= 150) )
+        {
+            cout<< "Sim" <<endl;
+        }
+        else
+        {
+            cout<< "Nao" <<endl;
+        }
+    }
 
 
 
