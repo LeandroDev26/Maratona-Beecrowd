e58741293563ebb3db45a2a2436ce3c956c54349
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-17 08:08:51 -0300

    feat

--- Exercises-Beecrowd/3042 - Desviando de Arvores de Natal/main.cpp
+++ Exercises-Beecrowd/3042 - Desviando de Arvores de Natal/main.cpp
@@ -1,26 +1,61 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int main()
 {
+    int m, l, c, r;
+    int INF = 999999;
 
-    int m, l, c, r, custo_esquerda_Atual = 0, custo_direita_Atual = 0, custo_centro_Atual = 0;
-    cin>> m ;
-
-    for(int i = 0; i < m; i++)
+    while (cin >> m && m != 0)
     {
-        cin>> l >> c >> r;
+
+        int custoEsq = 1, custoCen = 0, custoDir = 1;
+
+        for(int i = 0; i < m; i++)
+        {
+            cin >> l >> c >> r;
 
 
+            int novoEsq, novoCen, novoDir;
+
+            if (l == 1)
+            {
+                novoEsq = INF;
+            }
+            else
+            {
+                novoEsq = min(custoEsq, min(custoCen + 1, custoDir + 2));
+            }
+            if (c == 1)
+            {
+                novoCen = INF;
+            }
+            else
+            {
+                novoCen= min(custoCen, min(custoEsq + 1, custoDir + 1));
+            }
+            if (r == 1)
+            {
+                novoDir = INF;
+            }
+            else
+            {
+                novoDir = min(custoDir, min(custoCen + 1, custoEsq+ 2));
+            }
+
+
+
+            custoEsq = novoEsq;
+            custoCen = novoCen;
+            custoDir = novoDir;
+        }
+
+         int menor = min(custoEsq, min(custoCen , custoDir));
+
+        // TODO: No final do for, descubra qual é o menor valor entre custoEsq, custoCen e custoDir
+        // TODO: Imprima esse menor valor usando cout
     }
-
-
-
-
-
-
-
 
     return 0;
 }
