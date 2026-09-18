76beceb312dd932230711789d41d772b2900b2c4
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-18 09:26:51 -0300

    feat

--- Exercises-Beecrowd/3053 - Jogo Dos Copos/main.cpp
+++ Exercises-Beecrowd/3053 - Jogo Dos Copos/main.cpp
@@ -6,8 +6,9 @@ using namespace std;
 int main()
 {
     int n , movimento = 0 ;
-    char inicio;
+    char inicio , posicaoAtual;
     cin>> n >> inicio;
+    posicaoAtual = inicio;
     for(int i = 0 ; i < n ; i++)
     {
 
