c00e422068ddd92b1b975b7dbca264dd4dc08489
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-17 21:17:14 -0300

    feat

--- Exercises-Beecrowd/3047 - A idade de Dona Monica/main.cpp
+++ Exercises-Beecrowd/3047 - A idade de Dona Monica/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int main()
 {
 
-    int m, a, b, c, tot;
+    int m, a, b, c, tot  , maisVelho = 0;
 
     cin>> m >> a >> b;
     tot = a + b;
     c = (m - tot );
-    cout<< c <<endl;
+    maisVelho = max(a , max(b , c));
+    cout<< maisVelho  <<endl;
+
     return 0;
 }
