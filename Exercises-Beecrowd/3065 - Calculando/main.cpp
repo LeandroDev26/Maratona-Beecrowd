ff8a3c5912edc6935e629642f0d1658e4a866c3c
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-19 22:02:50 -0300

    feat

--- Exercises-Beecrowd/3065 - Calculando/main.cpp
+++ Exercises-Beecrowd/3065 - Calculando/main.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
-int main()
-{
+int main() {
+    int m;
+    int teste = 1;
 
+    while (cin >> m && m != 0) {
+        string expressao;
+        cin >> expressao;
 
+        stringstream ss(expressao);
+        int resultado = 0;
+        int numero;
+        char operador;
 
+        ss >> resultado;
 
+        while (ss >> operador >> numero) {
+            if (operador == '+') {
+                resultado += numero;
+            } else if (operador == '-') {
+                resultado -= numero;
+            }
+        }
 
-
-
-
+        cout << "Teste " << teste++ << endl;
+        cout << resultado << endl;
+        cout << endl;
+    }
 
     return 0;
 }
