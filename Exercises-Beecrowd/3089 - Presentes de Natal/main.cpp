ed02a06e52ca8352ff148964f0f139f2a842be7b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-22 10:19:29 -0300

    feat

--- Exercises-Beecrowd/3089 - Presentes de Natal/main.cpp
+++ Exercises-Beecrowd/3089 - Presentes de Natal/main.cpp
@@ -10,14 +10,18 @@ int main()
     while (cin>> n && n != 0)
     {
         vector <int> listaDeValores;
-        int maior_valor = 0 , menor_valor = 9999;
+        int maior_valor = 0, menor_valor = 9999;
 
         for(int i = 0 ; i < n*2; i++)
         {
-           cin>> x ;
-           listaDeValores.push_back(x);
+            cin>> x ;
+            listaDeValores.push_back(x);
         }
 
+        for(int j = 0 ; j < n ; j++)
+        {
+
+        }
 
     }
 
