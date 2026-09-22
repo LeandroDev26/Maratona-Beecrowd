bf2c1ce207440edb628433a0c20621d20a72f884
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-22 10:15:18 -0300

    feat

--- Exercises-Beecrowd/3089 - Presentes de Natal/main.cpp
+++ Exercises-Beecrowd/3089 - Presentes de Natal/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-
-    int n ;
+    int n, x;
 
     while (cin>> n && n != 0)
     {
+        vector <int> listaDeValores;
 
+        for(int i = 0 ; i < n*2; i++)
+        {
 
+        }
 
 
     }
