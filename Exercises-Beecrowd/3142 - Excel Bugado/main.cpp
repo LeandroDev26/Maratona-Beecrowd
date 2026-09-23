752e6b73b15c39314621d8fd686f3954d4aaf0ad
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-23 08:47:32 -0300

    feat

--- Exercises-Beecrowd/3142 - Excel Bugado/main.cpp
+++ Exercises-Beecrowd/3142 - Excel Bugado/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
+    string s ;
+
+    while(cin>> s)
+    {
+        int soma = 0 ;
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            int valor = s[i] - 'A' + 1;
+            soma += valor;
+
+        }
+
+    }
 
 
 
