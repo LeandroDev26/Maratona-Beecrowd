5fc33f86624f5fcbe1f42d5bd5157f20e73a68e4
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-23 08:35:54 -0300

    feat

--- Exercises-Beecrowd/3140 - Copiando e Colando Codigo/main.cpp
+++ Exercises-Beecrowd/3140 - Copiando e Colando Codigo/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
 using namespace std;
 
@@ -7,12 +6,27 @@ int main()
 {
 
     string texto ;
+    bool imprimir = false;
 
     while (getline(cin,texto) )
     {
-        vector<string> Texto;
-        bool imprimir = false;
-        Texto.push_back(texto);
+
+        if(texto.find("<body>") != string::npos)
+        {
+            imprimir = true ;
+            continue;
+        }
+        else if(texto.find("</body>") != string::npos)
+        {
+            imprimir = false ;
+            continue;
+        }
+
+
+        if(imprimir)
+        {
+            cout<< texto <<endl;
+        }
 
 
     }
