e570bad6add0ade1859a69de12f20e10b0e779f2
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-23 09:12:22 -0300

    feat

--- Exercises-Beecrowd/3145 - Uma Jornada Inesperada/main.cpp
+++ Exercises-Beecrowd/3145 - Uma Jornada Inesperada/main.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
 
-
-    int n, x , totP , totD;
+    int n, x, totP, totD;
     cin>> n >> x ;
     totP = n + 2;
-
-
-
-
-
-
-
+    totD = x /totP;
+    cout<< fixed << setprecision(2) << totD << "\n";
 
     return 0;
 }
