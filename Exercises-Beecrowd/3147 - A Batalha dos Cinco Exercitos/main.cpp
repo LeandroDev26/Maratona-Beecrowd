b75b58d9033733f9c0027cab87b243fa588d4950
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-24 08:44:16 -0300

    feat

--- Exercises-Beecrowd/3147 - A Batalha dos Cinco Exercitos/main.cpp
+++ Exercises-Beecrowd/3147 - A Batalha dos Cinco Exercitos/main.cpp
@@ -4,21 +4,29 @@ using namespace std;
 
 int main()
 {
-    int  H, E, A, O, W, X, bem, mal;
-    cin>> H >> E >> A >> O >> W >> X;
-    bem = H + E + A + X;
-    mal = O + W;
+    int H, E, A, O, W, X;
+    if (!(cin >> H >> E >> A >> O >> W >> X)) return 0;
 
-    if(mal > bem)
+    int bem_inicial = H + E + A;
+    int mal = O + W;
+
+    if (bem_inicial > mal)
     {
-        cout<< "Sauron has returned." <<endl;
+        cout << "Middle-earth is safe." << endl;
     }
     else
     {
-        cout<< "Middle-earth is safe." <<endl;
+        int bem_com_aguias = bem_inicial + X;
+
+        if (bem_com_aguias >= mal)
+        {
+            cout << "Middle-earth is safe." << endl;
+        }
+        else
+        {
+            cout << "Sauron has returned." << endl;
+        }
     }
-
-
 
     return 0;
 }
