3c0bcfc69873e3ec52451638a1913bbf32c1590b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-24 19:10:22 -0300

    feat

--- Exercises-Beecrowd/3157 - Viva a Ciencia/main.cpp
+++ Exercises-Beecrowd/3157 - Viva a Ciencia/main.cpp
@@ -5,14 +5,8 @@ using namespace std;
 int main()
 {
 
-   string texto ;
-   getline(cin, texto);
-   cout<< "Nao se refuta ciencia com opiniao" <<endl;
-
-
-
-
-
-
+    string texto ;
+    getline(cin, texto);
+    cout<< "Nao se refuta ciencia com opiniao" <<endl;
     return 0;
 }
